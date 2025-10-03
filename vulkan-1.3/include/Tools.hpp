298822b3c35a0d25680a1ca96a939ad8e0f0af25
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-03 18:53:39 +1000

    1.[ADD]:add Vulkan struture init code

--- vulkan-1.3/include/Tools.hpp
+++ vulkan-1.3/include/Tools.hpp
@@ -83,6 +83,52 @@ inline static  VkImageSubresourceRange image_subresource_range(VkImageAspectFlag
           return subImage;
 }
 
+[[nodiscard]]
+inline static VkSemaphoreSubmitInfo semaphore_submit_info(VkPipelineStageFlags2 stageMask, VkSemaphore semaphore)
+{
+          VkSemaphoreSubmitInfo submitInfo{};
+          submitInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_SUBMIT_INFO;
+          submitInfo.pNext = nullptr;
+          submitInfo.semaphore = semaphore;
+          submitInfo.stageMask = stageMask;
+          submitInfo.deviceIndex = 0;
+          submitInfo.value = 1;
+
+          return submitInfo;
+}
+
+[[nodiscard]]
+inline static VkCommandBufferSubmitInfo command_buffer_submit_info(VkCommandBuffer cmd)
+{
+          VkCommandBufferSubmitInfo info{};
+          info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_SUBMIT_INFO;
+          info.pNext = nullptr;
+          info.commandBuffer = cmd;
+          info.deviceMask = 0;
+
+          return info;
+}
+
+[[nodiscard]]
+inline static VkSubmitInfo2 submit_info(VkCommandBufferSubmitInfo* cmd, VkSemaphoreSubmitInfo* signalSemaphoreInfo,
+          VkSemaphoreSubmitInfo* waitSemaphoreInfo)
+{
+          VkSubmitInfo2 info = {};
+          info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2;
+          info.pNext = nullptr;
+
+          info.waitSemaphoreInfoCount = waitSemaphoreInfo == nullptr ? 0 : 1;
+          info.pWaitSemaphoreInfos = waitSemaphoreInfo;
+
+          info.signalSemaphoreInfoCount = signalSemaphoreInfo == nullptr ? 0 : 1;
+          info.pSignalSemaphoreInfos = signalSemaphoreInfo;
+
+          info.commandBufferInfoCount = 1;
+          info.pCommandBufferInfos = cmd;
+
+          return info;
+}
+
 } // namespace tools
 } // namespace engine
 
