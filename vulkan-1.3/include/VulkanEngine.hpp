f916df802686ba1bbde7de69ca7e7011dff85397
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-03 18:55:07 +1000

    1.[ADD]:add vulkan instance & device core code 2.[ADD]:add run function with glfw with poll event to draw frame 3.[IMPL]:impl init code with vkb lib

---
+++ vulkan-1.3/include/VulkanEngine.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef _VULKAN_ENGINE_HPP_
+#define _VULKAN_ENGINE_HPP_
+#include <vector>
+#include <iostream>
+#include <Window.hpp>
+#include <GlobalDef.hpp>
+
+namespace engine {
+          class VulkanEngine {
+          public:
+                    VulkanEngine(Window & win, bool enableValidationLayer = true);
+                    virtual ~VulkanEngine();
+                    VulkanEngine(VulkanEngine&&) = delete;
+                    VulkanEngine& operator=(VulkanEngine&&) = delete;
+
+          public:
+                    void run();
+                    [[nodiscard]] std::vector<const char*>  getRequiredExtensions();
+
+          protected:
+                    //draw loop
+                    void draw();
+
+                    void init_vulkan();
+                    void init_swapchain();
+                    void init_commands();
+                    void init_sync();
+
+                    void destroy_sync();
+                    void destroy_commands();
+                    void destroy_swapchain();
+                    void destroy_vulkan();
+
+                    FrameData& get_current_frame();
+                    void switch_to_next_frame();
+
+          private:
+                    void create_swapchain(uint32_t width, uint32_t height);
+
+          private:
+                    bool isInit = false;
+                    bool enableValidationLayers_ = true;
+                    Window& window_;
+
+                    VkInstance instance_;	// Vulkan library handle
+                    VkDebugUtilsMessengerEXT debugMessenger_;	// Vulkan debug output handle
+                    VkPhysicalDevice physicalDevice_ = VK_NULL_HANDLE;	// GPU chosen as the default device
+
+                    VkDevice device_;	 // Vulkan device for commands
+                    VkSurfaceKHR surface_;	// Vulkan window surface    
+
+                    //SwapChain Part
+                    VkSwapchainKHR swapchain_;
+                    VkFormat swapchainImageFormat_ = VK_FORMAT_B8G8R8A8_UNORM;
+
+                    std::vector<VkImage> swapchainImages_;			//
+                    std::vector<VkImageView> swapchainImageViews_;	//
+                    VkExtent2D swapchainExtent_;
+
+                    //CommandBuffer Part
+                    unsigned int frameNumber_ = 0;
+                    std::vector<FrameData> frames_;
+
+                    VkQueue graphicsQueue_;
+                    uint32_t graphicsQueueFamily_;
+          };
+}
+#endif //_VULKAN_ENGINE_HPP_
