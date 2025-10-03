bd4fadabc2cb69328241647943ad352bcc606b3e
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-03 13:18:45 +1000

    1.[ADD]:add FrameData with renderfinished fence and two SEM before fence & commandbuffer 2.[ADD]:add double buffer def

---
+++ vulkan-1.3/include/GlobalDef.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _GLOBALDEF_HPP_
+#define _GLOBALDEF_HPP_
+#include <vulkan/vulkan.hpp>
+
+namespace engine{
+
+		  //Double Buffer
+		  constexpr unsigned int FRAMES_IN_FLIGHT = 2;
+
+		  struct FrameData {
+					VkFence _renderFinishedFence;
+					VkSemaphore _swapChainWait, _renderPresentKHRSignal;
+					VkCommandPool _commandPool;
+					VkCommandBuffer _mainCommandBuffer;
+		  };
+}
+
+#endif //_GLOBALDEF_HPP_
