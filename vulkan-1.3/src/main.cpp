12ae24f6658dda1bdc4a2d1e2c538df406565e04
Liupeter01 <Liupeter012001@outlook.com>
2025-10-03 20:37:42 +1000

    1.[ADD]:add try-catch easy for debug

--- vulkan-1.3/src/main.cpp
+++ vulkan-1.3/src/main.cpp
@@ -1,11 +1,17 @@
 #include <VulkanEngine.hpp>
+#include <exception>
 #include <iostream>
 
 int main() {
+  try {
 
-  engine::Window win{std::string("Vulkan-1.3"), 800, 600};
-  engine::VulkanEngine engine{win};
-  engine.run();
+    engine::Window win{std::string("Vulkan-1.3"), 800, 600};
+    engine::VulkanEngine engine{win};
+    engine.run();
+
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << "\n";
+  }
 
   return 0;
 }
