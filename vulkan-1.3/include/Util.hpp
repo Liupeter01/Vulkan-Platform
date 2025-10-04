00e535d4c939f8dac347c5c94222706dff59e1ff
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-04 13:31:01 +1000

    1.[ADD]:add copy image to image but size will not be the same

--- vulkan-1.3/include/Util.hpp
+++ vulkan-1.3/include/Util.hpp
@@ -5,7 +5,7 @@
 
 namespace engine {
 namespace util {
-void transition_image(VkCommandBuffer cmd, VkImage image,
+static inline void transition_image(VkCommandBuffer cmd, VkImage image,
                       VkImageLayout currentLayout, VkImageLayout newLayout) {
   VkImageMemoryBarrier2 imageBarrier{};
   imageBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2;
@@ -37,6 +37,38 @@ void transition_image(VkCommandBuffer cmd, VkImage image,
   depInfo.pImageMemoryBarriers = &imageBarrier;
 
   vkCmdPipelineBarrier2(cmd, &depInfo);
+}
+
+static inline void copy_image_to_image(VkCommandBuffer cmd, 
+          VkImage source, VkImage destination, 
+          VkExtent2D srcSize, VkExtent2D dstSize) {
+
+		  VkImageBlit2 blitRegion{};
+		  blitRegion.sType = VK_STRUCTURE_TYPE_IMAGE_BLIT_2;
+
+		  blitRegion.srcOffsets[1].x = srcSize.width;
+		  blitRegion.srcOffsets[1].y = srcSize.height;
+
+		  blitRegion.dstOffsets[1].x = dstSize.width;
+		  blitRegion.dstOffsets[1].y = dstSize.height;
+		  blitRegion.dstOffsets[1].z = blitRegion.srcOffsets[1].z = 1;
+
+		  blitRegion.srcSubresource.aspectMask = blitRegion.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		  blitRegion.srcSubresource.baseArrayLayer = blitRegion.dstSubresource.baseArrayLayer = 0;
+		  blitRegion.srcSubresource.layerCount = blitRegion.dstSubresource.layerCount = 1;
+		  blitRegion.srcSubresource.mipLevel = blitRegion.dstSubresource.mipLevel = 0;
+
+		  VkBlitImageInfo2 blitInfo{};
+		  blitInfo.sType = VK_STRUCTURE_TYPE_BLIT_IMAGE_INFO_2;
+		  blitInfo.dstImage = destination;
+		  blitInfo.dstImageLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+		  blitInfo.srcImage = source;
+		  blitInfo.srcImageLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+		  blitInfo.filter = VK_FILTER_LINEAR;
+		  blitInfo.regionCount = 1;
+		  blitInfo.pRegions = &blitRegion;
+
+		  vkCmdBlitImage2(cmd, &blitInfo);
 }
 } // namespace util
 } // namespace engine
