ccaccd25db71fac8dcbc27f04a1e0c17f94f6bb8
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-07 22:22:55 +1100

    1.[ADD]:add virtual function 2.[ADD]:impl draw function 3.[ADD]:add unique set_descriptors for derived class

--- vulkan-1.3/include/pipeline/ComputePipeline.hpp
+++ vulkan-1.3/include/pipeline/ComputePipeline.hpp
@@ -1,13 +1,10 @@
 #pragma once
 #ifndef _COMPUTE_PIPELINE_HPP_
 #define _COMPUTE_PIPELINE_HPP_
+#include <string>
 #include <Descriptors.hpp>
 #include <GlobalDef.hpp>
-#include <string>
-
-#define GLM_FORCE_RADIANS // no degresss
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
+#include <pipeline/PipelineBasic.hpp>
 
 namespace engine {
 
@@ -20,39 +17,35 @@ struct ComputeShaderPushConstants {
 
 inline namespace compute {
 
-struct ComputePipelinePacked {
-
+class ComputePipelinePacked: public PipelineBasic{
+public:
+          std::string name = " ComputePipelinePacked";
   ComputePipelinePacked(VkDevice device);
   virtual ~ComputePipelinePacked();
-  void init(VkImageView imageView);
-  void destroy();
-  void draw_compute(VkCommandBuffer &cmd, VkExtent2D drawExtent);
+  void set_descriptors(VkImageView imageView);
+  void init() override;
+  void destroy() override;
+  void draw(VkCommandBuffer cmd, VkExtent2D drawExtent, VkImageView imageView) override;
+  ComputeShaderPushConstants& getData() { return data; }
 
-  std::string name = " ComputePipelinePacked";
+protected:
+  
   ComputeShaderPushConstants data{};
 
 private:
-  DescriptorLayoutBuilder builder;
+  
   const std::vector<DescriptorAllocator::PoolSizeRatio> sizes{
       {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1}};
 
   // Initializing the layout and descriptors; store image, or vertex indicies
   DescriptorAllocator descriptorAllocator_;
-  VkDescriptorSet drawCompDescriptor_;
-  VkDescriptorSetLayout drawCompDescriptorLayout_;
+  VkDescriptorSet descriptor_;
+  VkDescriptorSetLayout descriptorLayout_;
 
-  VkPipeline gradientComputePipeline_;
-  VkPipelineLayout gradientComputePipelineLayout_;
-
-protected:
-  void init_descriptors(VkImageView imageView);
+private:
   void init_pipeline();
   void destroy_descriptors();
   void destroy_pipeline();
-
-private:
-  bool isInit_ = false;
-  VkDevice device_;
 };
 } // namespace compute
 } // namespace engine
--- vulkan-1.3/src/ComputePipeline.cpp
+++ vulkan-1.3/src/ComputePipeline.cpp
@@ -5,48 +5,49 @@ namespace engine {
 namespace compute {
 
 ComputePipelinePacked::ComputePipelinePacked(VkDevice device)
-    : device_(device), isInit_(false), descriptorAllocator_{device, 10, sizes},
-      builder{device} {}
+    : descriptorAllocator_{device}, PipelineBasic{device, PipelineType::COMPUTE}
+{}
 
 ComputePipelinePacked::~ComputePipelinePacked() { destroy(); }
 
-void ComputePipelinePacked::init(VkImageView imageView) {
-  init_descriptors(imageView);
-  init_pipeline();
-
-  isInit_ = true;
+void ComputePipelinePacked::init() {
+          init_pipeline();
 }
 
 void ComputePipelinePacked::destroy() {
-  destroy_pipeline();
-  destroy_descriptors();
-
-  isInit_ = false;
+          destroy_descriptors();
+          destroy_pipeline();
+  reset_init();
 }
 
 void ComputePipelinePacked::destroy_descriptors() {
   if (isInit_) {
     descriptorAllocator_.destroy_pool();
-    vkDestroyDescriptorSetLayout(device_, drawCompDescriptorLayout_, nullptr);
+    vkDestroyDescriptorSetLayout(device_, descriptorLayout_, nullptr);
   }
 }
 
 void ComputePipelinePacked::destroy_pipeline() {
   if (isInit_) {
-    vkDestroyPipelineLayout(device_, gradientComputePipelineLayout_, nullptr);
-    vkDestroyPipeline(device_, gradientComputePipeline_, nullptr);
+    vkDestroyPipelineLayout(device_, pipelineLayout_, nullptr);
+    vkDestroyPipeline(device_, pipeline_, nullptr);
   }
 }
 
-void ComputePipelinePacked::init_descriptors(VkImageView imageView) {
+void ComputePipelinePacked::set_descriptors(VkImageView imageView) {
 
-  drawCompDescriptorLayout_ =
-      builder.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE)
-          .build(VK_SHADER_STAGE_COMPUTE_BIT); // add bindings
+          {
+                    DescriptorLayoutBuilder builder{ device_ };
+                    descriptorLayout_ =
+                              builder.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE)
+                              .build(VK_SHADER_STAGE_COMPUTE_BIT); // add bindings
+          }
 
   // allocate a descriptor set for our draw image
-  drawCompDescriptor_ =
-      descriptorAllocator_.allocate(drawCompDescriptorLayout_);
+ descriptorAllocator_.init_pool(10, sizes);
+
+  descriptor_ =
+      descriptorAllocator_.allocate(descriptorLayout_);
 
   VkDescriptorImageInfo imgInfo{};
   imgInfo.imageLayout = VK_IMAGE_LAYOUT_GENERAL;
@@ -56,7 +57,7 @@ void ComputePipelinePacked::init_descriptors(VkImageView imageView) {
   drawImageWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
 
   drawImageWrite.dstBinding = 0;
-  drawImageWrite.dstSet = drawCompDescriptor_;
+  drawImageWrite.dstSet = descriptor_;
   drawImageWrite.descriptorCount = 1;
   drawImageWrite.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
   drawImageWrite.pImageInfo = &imgInfo;
@@ -74,14 +75,14 @@ void ComputePipelinePacked::init_pipeline() {
   VkPipelineLayoutCreateInfo computeLayout{};
   computeLayout.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
   computeLayout.pNext = nullptr;
-  computeLayout.pSetLayouts = &drawCompDescriptorLayout_;
+  computeLayout.pSetLayouts = &descriptorLayout_;
   computeLayout.setLayoutCount = 1;
 
   computeLayout.pushConstantRangeCount = 1;
   computeLayout.pPushConstantRanges = &pushConstant;
 
   vkCreatePipelineLayout(device_, &computeLayout, nullptr,
-                         &gradientComputePipelineLayout_);
+                         &pipelineLayout_);
 
   // load shader
   VkShaderModule computeDrawShader;
@@ -98,30 +99,29 @@ void ComputePipelinePacked::init_pipeline() {
   VkComputePipelineCreateInfo computePipelineCreateInfo{};
   computePipelineCreateInfo.sType =
       VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
-  computePipelineCreateInfo.layout = gradientComputePipelineLayout_;
+  computePipelineCreateInfo.layout = pipelineLayout_;
   computePipelineCreateInfo.stage = stageinfo;
 
   vkCreateComputePipelines(device_, VK_NULL_HANDLE, 1,
                            &computePipelineCreateInfo, nullptr,
-                           &gradientComputePipeline_);
+                           &pipeline_);
 
   vkDestroyShaderModule(device_, computeDrawShader, nullptr);
 }
 
-void ComputePipelinePacked::draw_compute(VkCommandBuffer &cmd,
-                                         VkExtent2D drawExtent) {
+void ComputePipelinePacked::draw(VkCommandBuffer cmd, VkExtent2D drawExtent, VkImageView imageView) {
 
   vkCmdBindPipeline(cmd, VkPipelineBindPoint::VK_PIPELINE_BIND_POINT_COMPUTE,
-                    gradientComputePipeline_);
+                    pipeline_);
 
-  vkCmdPushConstants(cmd, gradientComputePipelineLayout_,
+  vkCmdPushConstants(cmd, pipelineLayout_,
                      VK_SHADER_STAGE_COMPUTE_BIT, 0,
                      sizeof(ComputeShaderPushConstants), &data);
 
   // bind the descriptor set containing the draw image for the compute pipeline
   vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE,
-                          gradientComputePipelineLayout_, 0, 1,
-                          &drawCompDescriptor_, 0, nullptr);
+                          pipelineLayout_, 0, 1,
+                          &descriptor_, 0, nullptr);
 
   // execute the compute pipeline dispatch. We are using 16x16 workgroup size so
   // we need to divide by it
