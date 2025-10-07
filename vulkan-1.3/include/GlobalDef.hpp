2f2550f27518294eb45c44bce7a47afafa8d4b87
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-07 18:22:55 +1100

    1.[REMOVE]:remove pushconstant struture to another file

--- vulkan-1.3/include/GlobalDef.hpp
+++ vulkan-1.3/include/GlobalDef.hpp
@@ -2,10 +2,7 @@
 #ifndef _GLOBALDEF_HPP_
 #define _GLOBALDEF_HPP_
 #include <vulkan/vulkan.hpp>
-
-#define GLM_FORCE_RADIANS // no degresss
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/glm.hpp>
+#include <vulkan/vulkan_core.h>
 
 namespace engine {
 
@@ -17,13 +14,6 @@ struct FrameData {
   VkSemaphore _swapChainWait, _renderPresentKHRSignal;
   VkCommandPool _commandPool;
   VkCommandBuffer _mainCommandBuffer;
-};
-
-struct ComputeShaderPushConstants {
-  glm::vec4 topLeft = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);     // Red
-  glm::vec4 topRight = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);    // Yellow
-  glm::vec4 bottomLeft = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);  // Blue
-  glm::vec4 bottomRight = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f); // Cyan
 };
 } // namespace engine
 
