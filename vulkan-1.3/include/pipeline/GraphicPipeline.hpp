03133248ec18df31103496b4df75dbb146cf2879
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-09 18:54:57 +1100

    1.[ADD]:add vmaallocator 2.[IMPL]:expose mesh's three steps interface to public 3.[ADD]:add  getImmSubmitFunctor to submit vertices & indicies info 4.[ADD]:add complicated mesh pipline to replace old simple triangle pipline create function 5.[UPDATE]:add pushconstant + bindindexbuffer + drawindexed to finished draw 6.[ADD]:add loadmesh function, which ONLY HANDLES load mesh to inside DS

--- vulkan-1.3/include/pipeline/GraphicPipeline.hpp
+++ vulkan-1.3/include/pipeline/GraphicPipeline.hpp
@@ -2,8 +2,11 @@
 #ifndef _GRAPHIC_PIPELINE_HPP_
 #define _GRAPHIC_PIPELINE_HPP_
 #include <GlobalDef.hpp>
+#include <functional>
 #include <pipeline/PipelineBasic.hpp>
+#include <mesh/MeshBuffers.hpp>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 namespace engine {
@@ -64,24 +67,48 @@ inline namespace graphic {
 class GraphicPipelinePacked : public PipelineBasic {
 public:
   std::string name = " GraphicPipelinePacked";
-  GraphicPipelinePacked(VkDevice device);
+  GraphicPipelinePacked(VkDevice device, VmaAllocator allocator);
   virtual ~GraphicPipelinePacked();
 
   GraphicPipelinePacked(const GraphicPipelinePacked &) = delete;
   GraphicPipelinePacked &operator=(const GraphicPipelinePacked &) = delete;
   void init() override;
   void destroy() override;
+  void load_mesh(const Mesh& mesh);
   void draw(VkCommandBuffer cmd, VkExtent2D drawExtent,
             VkImageView imageView) override;
+
+  void submitMesh(VkCommandBuffer cmd);
+  void flushUpload(VkFence fence);
+
+  std::function<void(VkCommandBuffer)> getImmSubmitFunctor();
+
+  template<typename T>
+  inline static constexpr VkIndexType getIndexType() {
+            using Decayed = std::decay_t<T>;
+            if constexpr (std::is_same_v<Decayed, uint32_t>) {
+                      return VK_INDEX_TYPE_UINT32;
+            }
+            else if constexpr (std::is_same_v<Decayed, uint16_t>) {
+                      return VK_INDEX_TYPE_UINT16;
+            }
+            else if constexpr (std::is_same_v<Decayed, uint8_t>) {
+                      return VK_INDEX_TYPE_UINT8;
+            }
+            else {
+                      static_assert(false, "Unsupported index type");
+            }
+  }
 
 protected:
   void init_pipeline();
   void destroy_pipeline();
 
-  //// Initializing the layout and descriptors; store image, or vertex indicies
-  // DescriptorAllocator descriptorAllocator_;
-  // VkDescriptorSet drawCompDescriptor_;
-  // VkDescriptorSetLayout drawCompDescriptorLayout_;
+private:
+          void init_triangle_pipline();
+          void init_mesh_pipline();
+
+          std::unique_ptr<mesh::GPUGeoMeshBuffers> mesh_ = nullptr;
 };
 } // namespace graphic
 } // namespace engine
