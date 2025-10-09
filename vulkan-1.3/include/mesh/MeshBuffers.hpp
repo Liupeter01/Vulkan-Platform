6c7a31316d7b1ef92f6720f973627fcddb15bf93
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-09 18:51:26 +1100

    1.[ADD]:add def for vertex 2.[ADD]:add Mesh def with vertics & indicies 3.[ADD]:add composition allocatedbuffer to GPUGeoMeshBuffers 4.[FEATURE]:buffer upload involving three steps: create & submit as immed commandbuffer & flushSync

---
+++ vulkan-1.3/include/mesh/MeshBuffers.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef _MESH_BUFFERS_HPP_
+#define _MESH_BUFFERS_HPP_
+#include <vector>
+#include <GlobalDef.hpp>
+
+namespace engine {
+          struct Vertex {
+                    glm::vec3 position;
+                    float uv_x;
+                    glm::vec3 normal;
+                    float uv_y;
+                    glm::vec4 color;
+          };
+
+          struct Mesh {
+                    std::vector<Vertex> vertices;
+                    std::vector<uint32_t> indices;
+          };
+
+          inline namespace mesh {
+                    struct GPUGeoMeshBuffers {
+                              GPUGeoMeshBuffers(VkDevice device,VmaAllocator allocator);
+                              virtual ~GPUGeoMeshBuffers();
+
+                              AllocatedBuffer indexBuffer;
+                              AllocatedBuffer vertexBuffer;
+
+                              std::vector<Vertex> vertex_;
+                              std::vector<uint32_t> indicies_;
+                              VkDeviceAddress vertexBufferAddress{ };
+
+                              GPUGeoMeshBuffers& createMesh( const Mesh& mesh);
+                              void submitMesh(VkCommandBuffer cmd);
+                              void flushUpload(VkFence fence);
+
+                    private:
+                              bool isinit = false;
+                              VkDevice device_;
+                              VmaAllocator allocator_;
+
+                              bool  pendingUpload_ = false;
+                              AllocatedBuffer staging;
+                    };
+
+                    struct GPUGeoPushConstants {
+                              glm::mat4 matrix;
+                              VkDeviceAddress vertexBuffer;
+                    };
+          }
+}
+
+#endif //_MESH_BUFFERS_HPP_
---
+++ vulkan-1.3/src/MeshBuffers.cpp
@@ -0,0 +1,96 @@
+#include <mesh/MeshBuffers.hpp>
+
+namespace engine {
+
+          namespace mesh {
+                    GPUGeoMeshBuffers::GPUGeoMeshBuffers(
+                              VkDevice device,
+                              VmaAllocator allocator)
+                              : device_{ device }
+                              , allocator_(allocator)
+                              , indexBuffer{ allocator }
+                              , vertexBuffer{ allocator }
+                              , staging{allocator}
+                              , isinit(false)
+                    { }
+
+                    GPUGeoMeshBuffers::~GPUGeoMeshBuffers() {
+                              if (isinit) {
+                                        indexBuffer.destroy();
+                                        vertexBuffer.destroy();
+                                        staging.destroy();
+                                        isinit = false;
+                              }
+                    }
+
+                    GPUGeoMeshBuffers& GPUGeoMeshBuffers::createMesh(
+                              const Mesh& mesh){
+
+                              const std::size_t vertexBufferSize = mesh.vertices.size() * sizeof(mesh.vertices[0]);
+                              const std::size_t indiciesBufferSize = mesh.indices.size() * sizeof(mesh.indices[0]);
+
+                              vertex_ =  mesh.vertices;
+                              indicies_ = mesh.indices;
+
+                              vertexBuffer.create(vertexBufferSize,
+                                        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+                                        VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+
+                                        //Shader Device Addr
+                                        VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
+
+                                        VMA_MEMORY_USAGE_GPU_ONLY);
+
+                              indexBuffer.create(indiciesBufferSize,
+                                        VK_BUFFER_USAGE_INDEX_BUFFER_BIT |
+                                        VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+
+                                        VMA_MEMORY_USAGE_GPU_ONLY);
+
+                              //find the adress of the vertex buffer
+                              VkBufferDeviceAddressInfo deviceAdressInfo{};
+                              deviceAdressInfo.sType = VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO;
+                              deviceAdressInfo.buffer = vertexBuffer.buffer;
+                              vertexBufferAddress = vkGetBufferDeviceAddress(device_, &deviceAdressInfo);
+                              return*this;
+                    }
+
+                    void GPUGeoMeshBuffers::submitMesh(VkCommandBuffer cmd) {
+                              staging.destroy();
+
+                              const std::size_t vertexBufferSize = vertex_.size() * sizeof(vertex_[0]);
+                              const std::size_t indiciesBufferSize = indicies_.size() * sizeof(indicies_[0]);
+
+                              staging.create(indiciesBufferSize + vertexBufferSize,
+                                        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                                        VMA_MEMORY_USAGE_CPU_ONLY);
+
+                              void* src = staging.map();
+                              memcpy(reinterpret_cast<unsigned char*>(src), vertex_.data(), vertexBufferSize);
+                              memcpy(reinterpret_cast<unsigned char*>(src) + vertexBufferSize, indicies_.data(), indiciesBufferSize);
+                              staging.unmap();
+
+                              VkBufferCopy vertexCopy{  };
+                              vertexCopy.size = vertexBufferSize;
+
+                              VkBufferCopy indexCopy{  };
+                              indexCopy.srcOffset = vertexBufferSize;
+                              indexCopy.size = indiciesBufferSize;
+
+                              vkCmdCopyBuffer(cmd, staging.buffer, vertexBuffer.buffer, 1, &vertexCopy);
+                              vkCmdCopyBuffer(cmd, staging.buffer, indexBuffer.buffer, 1, &indexCopy);
+
+                              pendingUpload_ = true;
+                    }
+
+                    void GPUGeoMeshBuffers::flushUpload(VkFence fence) {
+                              if (!pendingUpload_) return;
+
+                              vkWaitForFences(device_, 1, &fence, true, std::numeric_limits<uint64_t>::max());
+
+                              staging.destroy();
+                              pendingUpload_ = false;
+                    }
+          }
+
+}
