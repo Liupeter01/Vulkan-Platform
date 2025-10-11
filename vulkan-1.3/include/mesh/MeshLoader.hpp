243fa777d53fed3a8e90de130f3caea4cad0ea47
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-11 13:02:10 +1100

    1.[ADD]:add GeoSurface for primitives & composite GPUGeoMeshBuffers(indicies & vertices) to MeshAsset 2.[ADD]:add create function for current mesh 3.[ADD]:add static gltfmeshes loader with fastgltf lib

---
+++ vulkan-1.3/include/mesh/MeshLoader.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#ifndef _MESH_LOADER_HPP_
+#define _MESH_LOADER_HPP_
+#include <optional>
+#include <filesystem>
+#include <mesh/MeshBuffers.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+
+namespace engine {
+          namespace mesh {
+
+                    struct GeoSurface {
+                              uint32_t startIndex{};
+                              uint32_t count{};
+                    };
+
+                    struct MeshAsset {
+                              MeshAsset(VkDevice device, VmaAllocator allocator)
+                                        :meshBuffers{ device, allocator }
+                              { }
+
+                              void createAsset(std::vector<Vertex>&& vertices, std::vector<uint32_t>&& indices) {
+                                       meshBuffers.createMesh(std::move(vertices), std::move(indices));
+                              }
+
+                              void submitMesh(VkCommandBuffer cmd) {
+                                        meshBuffers.submitMesh(cmd);
+                              }
+                              void flushUpload(VkFence fence) {
+                                        meshBuffers.flushUpload(fence);
+                              }
+
+                              VkDeviceAddress getVertexDeviceAddr() {
+                                        return meshBuffers.vertexBufferAddress;
+                              }
+
+                              std::string meshName;
+                              std::vector<GeoSurface> meshSurfaces;
+                              GPUGeoMeshBuffers meshBuffers;
+
+                              /* DEBUG ONLY! display the vertex normals*/
+                              static constexpr bool OverrideColors = true;
+
+                              [[nodiscard]] 
+                              static
+                              std::optional <std::vector<std::shared_ptr<MeshAsset>>>
+                              loadGltfMeshes(VkDevice device, VmaAllocator allocator, std::filesystem::path filePath);
+                    };
+          }
+}
+
+#endif //_MESH_LOADER_HPP_
---
+++ vulkan-1.3/src/MeshLoader.cpp
@@ -0,0 +1,122 @@
+#include <tiny_obj_loader.h>
+#include <mesh/MeshLoader.hpp>
+#include <fastgltf/glm_element_traits.hpp>
+#include <fastgltf/tools.hpp>
+#include <fastgltf/types.hpp>
+#include <fastgltf/core.hpp>
+#include <iostream>
+#include <Tools.hpp>
+
+namespace engine {
+          namespace mesh {
+
+                    std::optional <std::vector<std::shared_ptr<MeshAsset>>>
+                    MeshAsset::loadGltfMeshes(VkDevice device, VmaAllocator allocator, std::filesystem::path filePath) {
+
+                              fastgltf::Asset gltf;
+                              fastgltf::Parser parser{};
+
+                              auto gltfData = fastgltf::GltfDataBuffer::FromPath(filePath);
+
+                              constexpr auto gltfOptions = fastgltf::Options::LoadExternalBuffers;
+
+                              auto load_gltf = parser.loadGltfBinary(gltfData.get(), filePath.parent_path(), gltfOptions);
+                              if (!load_gltf) {
+                                        std::cerr << "Failed to load GLB file: " <<  fastgltf::to_underlying(load_gltf.error()) << "\n";
+                                        return std::nullopt;
+                              }
+
+                              gltf = std::move(load_gltf.get());
+
+                              std::vector<std::shared_ptr<MeshAsset>> meshes;
+                              for (const fastgltf::Mesh& mesh : gltf.meshes) {
+
+                                        MeshAsset meshAssets{ device, allocator };
+                                        meshAssets.meshName = mesh.name;
+
+                                        std::vector<uint32_t> indices;
+                                        std::vector<Vertex> vertices;
+
+                                        for (auto&& primitive : mesh.primitives) {
+                                                  GeoSurface newSurface{};
+                                                  newSurface.startIndex = static_cast<uint32_t>(indices.size());
+                                                  newSurface.count = static_cast<uint32_t>(gltf.accessors[primitive.indicesAccessor.value()].count);
+
+                                                  size_t initial_vtx = vertices.size();
+
+                                                  // load indexes
+                                                  {
+                                                            fastgltf::Accessor& indexaccessor = gltf.accessors[primitive.indicesAccessor.value()];
+                                                            indices.reserve(indices.size() + indexaccessor.count);
+
+                                                            fastgltf::iterateAccessor<std::uint32_t>(gltf, indexaccessor,
+                                                                      [&](std::uint32_t idx) {
+                                                                                indices.push_back(idx + initial_vtx);
+                                                                      });
+                                                  }
+
+                                                  // load vertex positions
+                                                  {
+                                                            fastgltf::Accessor& posAccessor = gltf.accessors[primitive.findAttribute("POSITION")->accessorIndex];
+                                                            vertices.resize(vertices.size() + posAccessor.count);
+
+                                                            fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, posAccessor,
+                                                                      [&](glm::vec3 v, size_t index) {
+                                                                                Vertex newvtx;
+                                                                                newvtx.position = v;
+                                                                                newvtx.normal = { 1, 0, 0 };
+                                                                                newvtx.color = glm::vec4{ 1.f };
+                                                                                newvtx.uv_x = 0;
+                                                                                newvtx.uv_y = 0;
+                                                                                vertices[initial_vtx + index] = newvtx;
+                                                                      });
+                                                  }
+
+                                                  // load vertex normals
+                                                  auto normals = primitive.findAttribute("NORMAL");
+                                                  if (normals != primitive.attributes.end()) {
+
+                                                            fastgltf::iterateAccessorWithIndex<glm::vec3>(gltf, gltf.accessors[(*normals).accessorIndex],
+                                                                      [&](glm::vec3 v, size_t index) {
+                                                                                vertices[initial_vtx + index].normal = v;
+                                                                      });
+                                                  }
+
+                                                  // load UVs
+                                                  auto uv = primitive.findAttribute("TEXCOORD_0");
+                                                  if (uv != primitive.attributes.end()) {
+
+                                                            fastgltf::iterateAccessorWithIndex<glm::vec2>(gltf, gltf.accessors[(*uv).accessorIndex],
+                                                                      [&](glm::vec2 v, size_t index) {
+                                                                                vertices[initial_vtx + index].uv_x = v.x;
+                                                                                vertices[initial_vtx + index].uv_y = v.y;
+                                                                      });
+                                                  }
+
+                                                  // load vertex colors
+                                                  auto colors = primitive.findAttribute("COLOR_0");
+                                                  if (colors != primitive.attributes.end()) {
+
+                                                            fastgltf::iterateAccessorWithIndex<glm::vec4>(gltf, gltf.accessors[(*colors).accessorIndex],
+                                                                      [&](glm::vec4 v, size_t index) {
+                                                                                vertices[initial_vtx + index].color = v;
+                                                                      });
+                                                  }
+
+                                                  meshAssets.meshSurfaces.push_back(std::move(newSurface));
+                                        }
+
+                                        if (OverrideColors) {
+                                                  for (Vertex& vtx : vertices) {
+                                                            vtx.color = glm::vec4(vtx.normal, 1.f);
+                                                  }
+                                        }
+
+                                        auto ptr = std::make_shared<MeshAsset>(std::move(meshAssets));
+                                        ptr->createAsset(std::move(vertices), std::move(indices));
+                                        meshes.emplace_back(ptr);
+                              }
+                              return meshes;
+                    }
+          }
+}
