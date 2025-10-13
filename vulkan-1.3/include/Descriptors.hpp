a8550ccbf481e70ead15c747f44d64e737518d71
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-13 20:45:14 +1100

    1.[RENAME]:single descriptorpool renamed as  DescriptorPoolManager, which only take responsibility for single pool 2.[ADD]:add DescriptorPoolAllocator, which deal with multiple decriptor pools and quite similar to nginx memory allocation method 3.[ADD]:add DescriptorWriter which holds both image/buffer Info and link to VkWriteDescriptorSet via pointer, we combine them to a single VkDescriptorSet by batch operation

--- vulkan-1.3/include/Descriptors.hpp
+++ vulkan-1.3/include/Descriptors.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef _DESCRIPTOR_HPP_
 #define _DESCRIPTOR_HPP_
+#include <deque>
 #include <vector>
 #include <vulkan/vulkan.hpp>
 
@@ -20,32 +21,83 @@ struct DescriptorLayoutBuilder {
   std::vector<VkDescriptorSetLayoutBinding> bindings;
 };
 
-struct DescriptorAllocator {
-  struct PoolSizeRatio {
-    VkDescriptorType type;
-    float ratio;
-  };
+struct PoolSizeRatio {
+          VkDescriptorType type;
+          float ratio;
+};
 
-  DescriptorAllocator(const DescriptorAllocator &) = delete;
-  DescriptorAllocator &operator=(const DescriptorAllocator &) = delete;
+struct DescriptorPoolManager {
+          DescriptorPoolManager(VkDevice device);
 
-  DescriptorAllocator(VkDevice device);
+          DescriptorPoolManager(const DescriptorPoolManager&) = delete;
+          DescriptorPoolManager& operator=(const DescriptorPoolManager&) = delete;
 
-  virtual ~DescriptorAllocator();
+          DescriptorPoolManager(DescriptorPoolManager&& other) noexcept;
+          DescriptorPoolManager& operator=(DescriptorPoolManager&& other) noexcept;
 
-  void init_pool(uint32_t maxSets,
-                 const std::vector<PoolSizeRatio> &poolRatios);
+          static DescriptorPoolManager create(VkDevice device,
+                    const uint32_t set_count,
+                    const std::vector<VkDescriptorPoolSize>& poolRatios);
 
-  void reset_pool();
-  void destroy_pool();
+          virtual ~DescriptorPoolManager();
+          void init(const uint32_t set_count,
+                    const std::vector<VkDescriptorPoolSize>& poolRatios);
 
-  [[nodiscard]] VkDescriptorSet allocate(VkDescriptorSetLayout layout);
+          void reset();
+          void destroy();
+
+          [[nodiscard]] std::pair<VkDescriptorSet, VkResult>  allocate(VkDescriptorSetLayout layout, void* pNext = VK_NULL_HANDLE);
+
+private:
+          bool isInit_ = false;
+          VkDevice device_ = VK_NULL_HANDLE;
+          VkDescriptorPool pool_ = VK_NULL_HANDLE;
+};
+
+struct DescriptorPoolAllocator {
+          DescriptorPoolAllocator(const DescriptorPoolAllocator&) = delete;
+          DescriptorPoolAllocator&operator=(const DescriptorPoolAllocator&) = delete;
+          DescriptorPoolAllocator(VkDevice device);
+
+  virtual ~DescriptorPoolAllocator();
+
+  void init(const uint32_t setCount, const std::vector<PoolSizeRatio>& poolSizeRatio);
+
+  void reset_pools();
+  void destroy_pools();
+
+  [[nodiscard]] VkDescriptorSet allocate(VkDescriptorSetLayout layout, void* pNext =VK_NULL_HANDLE);
+
+protected:
+          [[nodiscard]]  DescriptorPoolManager get_pool();
+          void create_pool(const uint32_t setCount, const std::vector<PoolSizeRatio>& poolSizeRatio);
+          void init_poolSize(const uint32_t setCount, const std::vector<PoolSizeRatio>& poolSizeRatio);
 
 private:
   bool isInit_ = false;
   VkDevice device_ = VK_NULL_HANDLE;
-  VkDescriptorPool pool_ = VK_NULL_HANDLE;
+  uint32_t setCount_{};
+  std::vector<PoolSizeRatio>poolSizeRatio_;
+  std::vector<VkDescriptorPoolSize> poolSizes_;
+  std::vector<DescriptorPoolManager> readyPools_;
+  std::vector<DescriptorPoolManager> invalidPools_;
 };
+
+struct DescriptorWriter {
+          DescriptorWriter(VkDevice device);
+          void clear();
+          void write_buffer(int binding, VkBuffer buffer, size_t size, size_t offset, VkDescriptorType type);
+          void write_image(int binding, VkImageView imageView, VkSampler sampler, VkImageLayout layout, VkDescriptorType type);
+          void update_set(VkDescriptorSet set);
+
+          std::deque<VkDescriptorImageInfo> imageInfos;
+          std::deque<VkDescriptorBufferInfo> bufferInfos;
+          std::vector<VkWriteDescriptorSet> writes;
+
+private:
+          VkDevice device_;
+};
+
 } // namespace engine
 
 #endif //_DESCRIPTOR_HPP_
