fe4cb8e3cc46a0cd9fa87207c33b3bf0fadfac89
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-16 21:49:15 +1100

    1.[ADD]:add GLTF PBR Metallic_Roughness material related def 2.[ADD]:add GLTF PBR texture required resoureces

---
+++ vulkan-1.3/include/material/GLTFPBR_Material.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef _GLTF_PBR_MATERIAL_
+#define _GLTF_PBR_MATERIAL_
+#include <GlobalDef.hpp>
+
+#define GLM_FORCE_RADIANS // no degresss
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+namespace engine {
+
+          enum class MaterialPass {
+                    UNDEFINED,
+                    OPAQUE,
+                    TRANSPARENT
+          } ;
+
+          struct MaterialConstants {
+                    glm::vec4 colorFactors;
+                    glm::vec4 metal_rough_factors;	 //R & B
+                    glm::vec4 extra[14];			//padding, we need it anyway for uniform buffers
+          };
+
+          struct MaterialResources {
+                    VkImageView colorImage{};
+                    VkImageView metalRoughImage{};
+
+                    VkSampler colorSampler{};
+                    VkSampler metalRoughSampler{};
+
+                    VkBuffer materialConstantsData{};
+                    size_t materialConstantsOffset{};
+          };
+}
+
+#endif
