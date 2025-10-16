9da0a8800ef70af4853fedeffdd32d02da9a22ab
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-16 21:47:52 +1100

    1.[ADD]:add opaque & transparent pipeline create function 2.[ADD]:create different graphic pipeline according to material pass 3.[ADD]:add indepedant class rather than pipelinebase class

---
+++ vulkan-1.3/include/pipeline/MaterialPipeline.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef _MATERIAL_PIPELINE_HPP_
+#define _MATERIAL_PIPELINE_HPP_
+#include <pipeline/GraphicPipelineBuilder.hpp>
+
+namespace engine {
+          enum class MaterialPass;
+
+          struct MaterialPipeline {
+                    MaterialPipeline(VkDevice device);
+                    virtual ~MaterialPipeline();
+
+                    void create(MaterialPass pass, const std::vector<VkDescriptorSetLayout>& layouts);
+                    void destroy();
+
+                    VkPipeline getPipeline() { return pipeline_; }
+                    VkPipelineLayout getPipelineLayout() { return pipelineLayout_; }
+
+          protected:
+                    void create_opaque_pipeline(GraphicPipelineBuilder& builder);
+                    void create_transparent_pipeline(GraphicPipelineBuilder& builder);
+
+          private:
+                    bool isinit_ = false;
+                    VkDevice device_ = VK_NULL_HANDLE;
+
+                    VkPipeline pipeline_ = VK_NULL_HANDLE;
+                    VkPipelineLayout pipelineLayout_ = VK_NULL_HANDLE;
+          };
+}
+
+#endif //_MATERIAL_PIPELINE_HPP_
