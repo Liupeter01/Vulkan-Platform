43d0aa02a2a47291b322e3f2c0acf51aecae6fc1
Liupeter01 <Liupeter012001@outlook.com>
2025-10-29 21:28:03 +1100

    1.[FORMATTING]

--- vulkan-1.3/include/nodes/ScenesManager.hpp
+++ vulkan-1.3/include/nodes/ScenesManager.hpp
@@ -1,34 +1,34 @@
 #pragma once
 #ifndef _SCENES_NODES_MANAGER_HPP_
 #define _SCENES_NODES_MANAGER_HPP_
-#include <string>
 #include <memory>
-#include <optional>
-#include <unordered_map>
 #include <nodes/scene/SceneNode.hpp>
+#include <optional>
+#include <string>
+#include <unordered_map>
 
 namespace engine {
-          class ScenesManager {
-          public:
-                    ScenesManager(VulkanEngine* eng);
-                    virtual ~ScenesManager();
+class ScenesManager {
+public:
+  ScenesManager(VulkanEngine *eng);
+  virtual ~ScenesManager();
 
-          public:
-                    void init();
-                    void destroy();
-                    void draw(const glm::mat4& parentMatrix, DrawContext& ctx);
+public:
+  void init();
+  void destroy();
+  void draw(const glm::mat4 &parentMatrix, DrawContext &ctx);
 
-          protected:
-                    void destroy_scene();
+protected:
+  void destroy_scene();
 
-          private:
-                    bool isinit = false;
-                    VulkanEngine* engine_{};
-                    std::unordered_map<
-                              /*scene name = */ std::string,
-                              /*scene nodes = */std::shared_ptr<NodeManager>>
-                              loadedScenes_;
-          };
+private:
+  bool isinit = false;
+  VulkanEngine *engine_{};
+  std::unordered_map<
+      /*scene name = */ std::string,
+      /*scene nodes = */ std::shared_ptr<NodeManager>>
+      loadedScenes_;
+};
 
 } // namespace engine
 
--- vulkan-1.3/src/ScenesManager.cpp
+++ vulkan-1.3/src/ScenesManager.cpp
@@ -3,35 +3,31 @@
 
 namespace engine {
 
-          ScenesManager::ScenesManager(VulkanEngine* eng)
-                    :engine_(eng)
-          {
-          }
+ScenesManager::ScenesManager(VulkanEngine *eng) : engine_(eng) {}
 
-          ScenesManager::~ScenesManager() {
-                    destroy();
-          }
+ScenesManager::~ScenesManager() { destroy(); }
 
-          void ScenesManager::init() {
-                    if (isinit) return;
-                    isinit = true;
-          }
+void ScenesManager::init() {
+  if (isinit)
+    return;
+  isinit = true;
+}
 
-          void ScenesManager::destroy() {
-                    if (isinit) {
-                              destroy_scene();
-                              isinit = false;
-                    }
-          }
+void ScenesManager::destroy() {
+  if (isinit) {
+    destroy_scene();
+    isinit = false;
+  }
+}
 
-          void ScenesManager::draw(const glm::mat4& parentMatrix, DrawContext& ctx) {
-                    for (auto& [scene_name, scene] : loadedScenes_) {
-                              scene->Draw(parentMatrix, ctx);
-                    }
-          }
+void ScenesManager::draw(const glm::mat4 &parentMatrix, DrawContext &ctx) {
+  for (auto &[scene_name, scene] : loadedScenes_) {
+    scene->Draw(parentMatrix, ctx);
+  }
+}
 
-          void ScenesManager::destroy_scene() {
-                    vkDeviceWaitIdle(engine_->device_);
-                    loadedScenes_.clear();
-          }
+void ScenesManager::destroy_scene() {
+  vkDeviceWaitIdle(engine_->device_);
+  loadedScenes_.clear();
+}
 } // namespace engine
