4c0ac5e4175cbc9fa544b7ba52bd4e27fb33c5f8
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-29 21:24:59 +1100

    1.[ADD]:add scenemanager to manage std::shared_ptr<nodemanager> smart pointer (the type could be nodemanager & scenenode) 2.[NOT IMPL]:CRUD function

---
+++ vulkan-1.3/src/ScenesManager.cpp
@@ -0,0 +1,37 @@
+#include <VulkanEngine.hpp>
+#include <nodes/ScenesManager.hpp>
+
+namespace engine {
+
+          ScenesManager::ScenesManager(VulkanEngine* eng)
+                    :engine_(eng)
+          {
+          }
+
+          ScenesManager::~ScenesManager() {
+                    destroy();
+          }
+
+          void ScenesManager::init() {
+                    if (isinit) return;
+                    isinit = true;
+          }
+
+          void ScenesManager::destroy() {
+                    if (isinit) {
+                              destroy_scene();
+                              isinit = false;
+                    }
+          }
+
+          void ScenesManager::draw(const glm::mat4& parentMatrix, DrawContext& ctx) {
+                    for (auto& [scene_name, scene] : loadedScenes_) {
+                              scene->Draw(parentMatrix, ctx);
+                    }
+          }
+
+          void ScenesManager::destroy_scene() {
+                    vkDeviceWaitIdle(engine_->device_);
+                    loadedScenes_.clear();
+          }
+} // namespace engine
