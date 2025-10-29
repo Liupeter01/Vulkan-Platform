2dc378d47bc2776cbb6b85b0fa400eed98d0c088
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-29 20:05:40 +1100

    1.[ADD]:add error checker(filepath & conf & engine pointer) 2.[ADD]:add temp data structures to store mesh/basenode/texture/materialinstance for index 3.[ADD]:we use set functions to set inner data, and use build() function at final stage to create the SceneNode

---
+++ vulkan-1.3/include/builder/SceneNodeBuilder.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#ifndef _SCENE_NODE_BUILDER_HPP_
+#define _SCENE_NODE_BUILDER_HPP_
+#include <filesystem>
+#include <nodes/scene/SceneNode.hpp>
+
+namespace engine {
+
+          struct SceneNodeBuilder {
+
+                    SceneNodeBuilder(VulkanEngine* eng);
+                    SceneNodeBuilder& set_options(fastgltf::Options option = 
+                              fastgltf::Options::DontRequireValidAssetMember |
+                              fastgltf::Options::AllowDouble |
+                              fastgltf::Options::LoadExternalBuffers);
+
+                    SceneNodeBuilder& set_filepath(std::filesystem::path filePath);
+                    SceneNodeBuilder& set_config(const node::SceneNodeConf& conf);
+                    std::optional<std::shared_ptr<node::SceneNode>> build();
+
+          protected:
+                    static VkFilter extract_filter(fastgltf::Filter filter);
+                    static VkSamplerMipmapMode extract_mipmap_mode(fastgltf::Filter filter);
+
+                    void processSamplers(fastgltf::Asset& gltf);
+                    void processImages(fastgltf::Asset& gltf);
+                    void processMaterials(fastgltf::Asset& gltf);
+                    void processMeshes(fastgltf::Asset& gltf);
+                    void processNodes(fastgltf::Asset& gltf);
+                    void processRelation(fastgltf::Asset& gltf);
+
+          protected:
+                    fastgltf::Options options_{};
+                    std::filesystem::path filepath_{};
+                    std::optional<node::SceneNodeConf> conf_{};
+
+                    // temporal arrays for all the objects to use while creating the GLTF data
+                    std::vector<std::shared_ptr<MeshAsset>> meshes_;
+                    std::vector<std::shared_ptr<node::BaseNode>> nodes_;
+                    std::vector< std::shared_ptr<AllocatedTexture>> images_;
+                    std::vector<std::shared_ptr<MaterialInstance>> materials_;
+
+          private:
+                    VulkanEngine* engine_{};
+                    std::shared_ptr<node::SceneNode> scene{};
+          };
+}
+
+#endif //_SCENE_NODE_BUILDER_HPP_
