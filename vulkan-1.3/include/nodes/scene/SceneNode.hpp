f149f66db0de926fb760105fe7069388dc293d85
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-10-29 19:58:16 +1100

    1.[ADD]:add SceneNode class and inhertied from NodeMnager for its MeshAsset & BaseNode mapping structures 2.[FEATURE]:sceneroot, meshes, nodes are embedded inside scenenode 3.[ADD]:expose Draw interface(however delegate to NodeManager layer) 4.[ADD]:add allocatedtexture Materialinstance vksampler to support PBR material

---
+++ vulkan-1.3/include/nodes/scene/SceneNode.hpp
@@ -0,0 +1,91 @@
+#pragma once
+#ifndef _SCENE_NODE_HPP_
+#define _SCENE_NODE_HPP_
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <GlobalDef.hpp>
+#include <unordered_map>
+#include <Descriptors.hpp>
+#include <fastgltf/core.hpp>
+#include <fastgltf/tools.hpp>
+#include <fastgltf/types.hpp>
+#include <nodes/scene/NodeManager.hpp>
+#include <nodes/core/BaseNode.hpp>
+#include <mesh/MeshLoader.hpp>
+#include <fastgltf/glm_element_traits.hpp>
+#include <material/GLTFMetallic_Roughness.hpp>
+
+namespace engine {
+
+          struct SceneNodeBuilder;
+
+          namespace node {
+
+                    struct SceneNodeConf {
+                              std::string sceneRootName = "__custom_scene__";
+                              uint32_t setCount;
+                              std::vector<PoolSizeRatio> poolSizeRatio;
+                              VkDescriptorSetLayout globalSceneLayout;
+                    };
+
+                    class SceneNode : public NodeManager {
+
+                              /* 
+                              * Those Data Structures Are already Included in NodeManager
+                              * std::shared_ptr<node::BaseNode> sceneRoot_ = nullptr;
+                              * std::unordered_map<std::string, std::shared_ptr<MeshAsset>> meshes_;
+                              * std::unordered_map<std::string, std::shared_ptr<node::BaseNode>> nodes_;
+                              */
+
+                    public:
+                              friend struct SceneNodeBuilder;
+                              SceneNode(VulkanEngine* eng);
+                              virtual ~SceneNode();
+
+                    public:
+                              void init(const SceneNodeConf& conf);
+                              void destroy();
+                              void reset_allocator_pools();
+                              void Draw(const glm::mat4& topMatrix, DrawContext& ctx) override;
+
+                    protected:
+                              void init_allocator(const uint32_t setCount,
+                                        const std::vector<PoolSizeRatio>& poolSizeRatio);
+
+                              void init_material(VkDescriptorSetLayout globalSceneLayout);
+
+                              void destroy_material();
+                              void destroy_allocator();
+                              void destroy_sampler();
+
+                              bool insert_to_lookup_table(std::shared_ptr<node::BaseNode>& node,
+                                        std::string_view parentPath);
+
+                    private:
+                              inline static const std::vector<PoolSizeRatio> sizes = {
+                                        { VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 3 },
+                                        { VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 3 },
+                                        { VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1 }
+                              };
+
+                              bool isinit = false;
+                              VulkanEngine* engine_;
+                              DescriptorPoolAllocator pool_;
+
+                               // storage for all the data on a given glTF file
+                              std::unordered_map<std::string, std::shared_ptr<AllocatedTexture>> images_;
+                              std::unordered_map < std::string, std::shared_ptr<MaterialInstance>> materials_;
+
+                              std::unique_ptr<GLTFMetallic_Roughness> metalRoughMaterial = nullptr;
+
+                              std::shared_ptr<AllocatedBuffer> materialBuffer = nullptr;
+
+                              std::vector<VkSampler> samplers;
+                    };
+
+          } // namespace node
+} // namespace engine
+
+#endif //_SCENE_NODE_HPP_
