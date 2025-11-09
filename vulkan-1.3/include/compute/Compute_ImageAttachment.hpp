f1f06b3fc22c107512cc2b3895d4187b76ba3013
Liupeter01 <Liupeter012001@outlook.com>
2025-11-09 21:14:51 +1100

    1.[FIX]:.template must be explicitly specifiy

--- vulkan-1.3/include/compute/Compute_ImageAttachment.hpp
+++ vulkan-1.3/include/compute/Compute_ImageAttachment.hpp
@@ -5,81 +5,85 @@
 
 namespace engine {
 
-          struct ComputeShaderPushConstants {
-                    glm::vec4 topLeft = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);     // Red
-                    glm::vec4 topRight = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);    // Yellow
-                    glm::vec4 bottomLeft = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);  // Blue
-                    glm::vec4 bottomRight = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f); // Cyan
-          };
+struct ComputeShaderPushConstants {
+  glm::vec4 topLeft = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);     // Red
+  glm::vec4 topRight = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);    // Yellow
+  glm::vec4 bottomLeft = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);  // Blue
+  glm::vec4 bottomRight = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f); // Cyan
+};
 
-          struct ImageAttachmentResources {
-                    VkImageView colorImageView{};
-          };
+struct ImageAttachmentResources {
+  VkImageView colorImageView{};
+};
 
 inline namespace compute {
 
-template<typename PushConstantType = ComputeShaderPushConstants,
-                  typename ResourcesType = ImageAttachmentResources>
+template <typename PushConstantType = ComputeShaderPushConstants,
+          typename ResourcesType = ImageAttachmentResources>
 
-class Compute_ImageAttachment :
-          public Compute_EffectBase< PushConstantType,  ResourcesType>{
+class Compute_ImageAttachment
+    : public Compute_EffectBase<PushConstantType, ResourcesType> {
 
 public:
-          Compute_ImageAttachment(VkDevice device)
-                    : Compute_EffectBase< PushConstantType, ResourcesType>(device)
-          { }
+  Compute_ImageAttachment(VkDevice device)
+      : Compute_EffectBase<PushConstantType, ResourcesType>(device) {}
 
-          void init() override {
-                    if (isinit_) return;
+  void init() override {
+    if (this->isinit_)
+      return;
 
-                    DescriptorLayoutBuilder builder{ this->device_ };
-                    this->computeLayout_ = builder
-                              .add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE)
-                              .build(VK_SHADER_STAGE_COMPUTE_BIT); // add bindings
+    DescriptorLayoutBuilder builder{this->device_};
+    this->computeLayout_ =
+        builder.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE)
+            .build(VK_SHADER_STAGE_COMPUTE_BIT); // add bindings
 
-                    auto layout = { this->computeLayout_ };
+    auto layout = {this->computeLayout_};
 
-                    this->defaultComputePipeline_.create<PushConstantType>(ComputePass::DEFAULT, layout);
-                    this->specialConstantPipeline_.create<PushConstantType>(ComputePass::SPECIALCONSTANT, layout);
-                    isinit_ = true;
-          }
+    this->defaultComputePipeline_.template create<PushConstantType>(
+        ComputePass::DEFAULT, layout);
+    this->specialConstantPipeline_.template create<PushConstantType>(
+        ComputePass::SPECIALCONSTANT, layout);
+    this->isinit_ = true;
+  }
 
-          void dispatch(VkCommandBuffer cmd, const ComputeInstance& ins) override{
+  void dispatch(VkCommandBuffer cmd, const ComputeInstance &ins) override {
 
-                    vkCmdBindPipeline(cmd, VkPipelineBindPoint::VK_PIPELINE_BIND_POINT_COMPUTE,
-                              ins.pipeline->getPipeline());
+    vkCmdBindPipeline(cmd, VkPipelineBindPoint::VK_PIPELINE_BIND_POINT_COMPUTE,
+                      ins.pipeline->getPipeline());
 
-                    vkCmdPushConstants(
-                              cmd, ins.pipeline->getPipelineLayout(), VK_SHADER_STAGE_COMPUTE_BIT, 0,
-                              sizeof(PushConstantType), &pushConstantData_);
+    vkCmdPushConstants(cmd, ins.pipeline->getPipelineLayout(),
+                       VK_SHADER_STAGE_COMPUTE_BIT, 0, sizeof(PushConstantType),
+                       &this->pushConstantData_);
 
-                    // bind the descriptor set containing the draw image for the compute pipeline
-                    vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE,
-                              ins.pipeline->getPipelineLayout(), 0, 1,
-                              &ins.computeSet, 0, nullptr);
+    // bind the descriptor set containing the draw image for the compute
+    // pipeline
+    vkCmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE,
+                            ins.pipeline->getPipelineLayout(), 0, 1,
+                            &ins.computeSet, 0, nullptr);
 
-                    // execute the compute pipeline dispatch. We are using 16x16 workgroup size so
-                    // we need to divide by it
+    // execute the compute pipeline dispatch. We are using 16x16 workgroup size
+    // so we need to divide by it
 
-                    vkCmdDispatch(cmd, dispatchGroups_.x, dispatchGroups_.y, dispatchGroups_.z);
-          }
+    vkCmdDispatch(cmd, this->dispatchGroups_.x, this->dispatchGroups_.y,
+                  this->dispatchGroups_.z);
+  }
 
-          ComputeInstance  generate_instance(
-                    ResourcesType& resources,
-                    DescriptorPoolAllocator& globalDescriptorAllocator) override {
+  ComputeInstance generate_instance(
+      ResourcesType &resources,
+      DescriptorPoolAllocator &globalDescriptorAllocator) override {
 
-                    writer_.clear();
+    this->writer_.clear();
 
-                    ComputeInstance ins{};
-                    ins.pipeline = &defaultComputePipeline_;
-                    ins.computeSet = globalDescriptorAllocator.allocate(computeLayout_);
+    ComputeInstance ins{};
+    ins.pipeline = &this->defaultComputePipeline_;
+    ins.computeSet = globalDescriptorAllocator.allocate(this->computeLayout_);
 
-                    writer_.write_image(0, resources.colorImageView, VK_NULL_HANDLE,
+    this->writer_.write_image(0, resources.colorImageView, VK_NULL_HANDLE,
                               VK_IMAGE_LAYOUT_GENERAL,
                               VK_DESCRIPTOR_TYPE_STORAGE_IMAGE);
-                    writer_.update_set(ins.computeSet);
-                    return ins;
-        }
+    this->writer_.update_set(ins.computeSet);
+    return ins;
+  }
 };
 } // namespace compute
 } // namespace engine
