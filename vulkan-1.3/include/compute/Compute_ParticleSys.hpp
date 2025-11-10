9f4376e98066ab7868ecaaef27742b2e37bc65e6
Liupeter01 <Liupeter012001@outlook.com>
2025-11-10 13:18:45 +1100

    1.[FORMATTING] 2.[ADD]:add header file to include allocated buffer

--- vulkan-1.3/include/compute/Compute_ParticleSys.hpp
+++ vulkan-1.3/include/compute/Compute_ParticleSys.hpp
@@ -1,126 +1,126 @@
 #pragma once
 #ifndef _COMPUTE_PARTICLESYS_HPP_
 #define _COMPUTE_PARTICLESYS_HPP_
+#include <GlobalDef.hpp>
 #include <array>
-#include <random>
 #include <compute/Compute_EffectBase.hpp>
+#include <random>
 
 namespace engine {
 
-          struct ParticlePushConstant {
-                    float     deltaTime{};   //
-                    uint32_t  frameIndex{};  // 
-                    uint32_t  _pad0{};       // 
-                    uint32_t  _pad1{};
-          };
+struct ParticlePushConstant {
+  float deltaTime{};     //
+  uint32_t frameIndex{}; //
+  uint32_t _pad0{};      //
+  uint32_t _pad1{};
+};
 
-          struct GPUParticle {
-                    glm::vec4 position;
-                    glm::vec4 velocity;
-                    glm::vec4 color;
-          };
+struct GPUParticle {
+  glm::vec4 position;
+  glm::vec4 velocity;
+  glm::vec4 color;
+};
 
-          struct ParticleSysDataBuffer {
-                    ParticleSysDataBuffer(VulkanEngine* eng);
-                    virtual ~ParticleSysDataBuffer();
-                    void destroy();
-                    void invalid();
-                    bool isValid() const;
-                    void create(const std::size_t particle_count);
-                    void swap() noexcept;
-                    void submit(VkCommandBuffer cmd);
-                    void flush(VkFence fence);
-                    VkBuffer get_in_buffer() const noexcept;
-                    VkBuffer get_out_buffer() const noexcept;
+struct ParticleSysDataBuffer {
+  ParticleSysDataBuffer(VulkanEngine *eng);
+  virtual ~ParticleSysDataBuffer();
+  void destroy();
+  void invalid();
+  bool isValid() const;
+  void create(const std::size_t particle_count);
+  void swap() noexcept;
+  void submit(VkCommandBuffer cmd);
+  void flush(VkFence fence);
+  VkBuffer get_in_buffer() const noexcept;
+  VkBuffer get_out_buffer() const noexcept;
 
-                    /*one is for particle in, another is for particle out;
-                    * buffer[0] will be the first one and it's going to swap in every frame
-                    */
-                    bool index = 0;
-                    std::size_t particleCount{ 0 };
-                    std::array<std::shared_ptr<AllocatedBuffer>, 2> buffers;
+  /*one is for particle in, another is for particle out;
+   * buffer[0] will be the first one and it's going to swap in every frame
+   */
+  bool index = 0;
+  std::size_t particleCount{0};
+  std::array<std::shared_ptr<AllocatedBuffer>, 2> buffers;
 
-          protected:
-                    std::random_device rd;
-                    std::mt19937 mt;
-                    std::uniform_real_distribution<float> rndDist;
+protected:
+  std::random_device rd;
+  std::mt19937 mt;
+  std::uniform_real_distribution<float> rndDist;
 
-                    //This is the staging buffer to init the first buffer slot
-                    AllocatedBuffer staging;
+  // This is the staging buffer to init the first buffer slot
+  AllocatedBuffer staging;
 
-                    void fill(void* buffer, const std::size_t particle_count);
+  void fill(void *buffer, const std::size_t particle_count);
 
-          private:
-                    bool isinit_ = false;
-                    VulkanEngine* engine_;
-                    bool pendingUpload_ = false;
-          };
+private:
+  bool isinit_ = false;
+  VulkanEngine *engine_;
+  bool pendingUpload_ = false;
+};
 
-          struct ParticleResources {
-                    VkImageView colorImage{};
+struct ParticleResources {
+  VkImageView colorImage{};
 
-                    //Ping Pong Swapping structure
-                    VkBuffer particlesIn;
-                    VkBuffer particlesOut;
-                    std::size_t bufferSize;
-          };
+  // Ping Pong Swapping structure
+  VkBuffer particlesIn;
+  VkBuffer particlesOut;
+  std::size_t bufferSize;
+};
 
-          inline namespace compute {
+inline namespace compute {
 
-                    template<typename PushConstantType = ParticlePushConstant,
-                              typename ResourcesType = ParticleResources >
-                    class Compute_ParticleSys        
-                              : public Compute_EffectBase< PushConstantType, ResourcesType> {
+template <typename PushConstantType = ParticlePushConstant,
+          typename ResourcesType = ParticleResources>
+class Compute_ParticleSys
+    : public Compute_EffectBase<PushConstantType, ResourcesType> {
 
-                    public:
+public:
+  Compute_ParticleSys(VkDevice device)
+      : Compute_EffectBase<PushConstantType, ResourcesType>(device) {}
 
-                              Compute_ParticleSys(VkDevice device)
-                                        : Compute_EffectBase< PushConstantType, ResourcesType>(device)
-                              {}
+  void init() override {
+    if (this->isinit_)
+      return;
 
-                              void init() override {
-                                        if (this->isinit_) return;
+    DescriptorLayoutBuilder builder{this->device_};
+    this->computeLayout_ =
+        builder
+            //.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE)
+            .add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER)
+            .add_binding(1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER)
+            .build(VK_SHADER_STAGE_COMPUTE_BIT); // add bindings
 
-                                        DescriptorLayoutBuilder builder{ this->device_ };
-                                        this->computeLayout_ = builder
-                                                  //.add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_IMAGE)
-                                                  .add_binding(0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER)
-                                                  .add_binding(1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER)
-                                                  .build(VK_SHADER_STAGE_COMPUTE_BIT); // add bindings
+    auto layout = {this->computeLayout_};
 
-                                        auto layout = { this->computeLayout_ };
+    this->defaultComputePipeline_.template create<PushConstantType>(
+        ComputePass::DEFAULT, layout);
+    this->specialConstantPipeline_.template create<PushConstantType>(
+        ComputePass::SPECIALCONSTANT, layout);
+    this->isinit_ = true;
+  }
 
-                                        this->defaultComputePipeline_.template create<PushConstantType>(ComputePass::DEFAULT, layout);
-                                        this->specialConstantPipeline_.template create<PushConstantType>(ComputePass::SPECIALCONSTANT, layout);
-                                        this->isinit_ = true;
-                              }
+  void dispatch(VkCommandBuffer cmd, const ComputeInstance &ins) override {}
 
-                              void dispatch(VkCommandBuffer cmd, const ComputeInstance& ins) override {
-                              }
+  ComputeInstance generate_instance(
+      ResourcesType &resources,
+      DescriptorPoolAllocator &globalDescriptorAllocator) override {
 
-                              ComputeInstance  generate_instance(
-                                        ResourcesType& resources,
-                                        DescriptorPoolAllocator& globalDescriptorAllocator) override {
+    this->writer_.clear();
 
-                                        this->writer_.clear();
+    ComputeInstance ins{};
+    ins.pipeline = &this->defaultComputePipeline_;
+    ins.computeSet = globalDescriptorAllocator.allocate(this->computeLayout_);
 
-                                        ComputeInstance ins{};
-                                        ins.pipeline = &this->defaultComputePipeline_;
-                                        ins.computeSet = globalDescriptorAllocator.allocate(this->computeLayout_);
+    this->writer_.write_buffer(0, resources.particlesIn, resources.bufferSize,
+                               0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
 
-                                        this->writer_.write_buffer(
-                                                  0, resources.particlesIn, resources.bufferSize,
-                                                  0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
+    this->writer_.write_buffer(1, resources.particlesOut, resources.bufferSize,
+                               0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
 
-                                       this->writer_.write_buffer(
-                                                  1, resources.particlesOut, resources.bufferSize,
-                                                  0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
-
-                                       this->writer_.update_set(ins.computeSet);
-                                        return ins;
-                              }
-                    };
-          } // namespace compute
+    this->writer_.update_set(ins.computeSet);
+    return ins;
+  }
+};
+} // namespace compute
 } // namespace engine
 
 #endif //_COMPUTE_PARTICLESYS_HPP_
--- vulkan-1.3/src/Compute_ParticleSys.cpp
+++ vulkan-1.3/src/Compute_ParticleSys.cpp
@@ -1,122 +1,118 @@
-#include <spdlog/fmt/fmt.h>
 #include <VulkanEngine.hpp>
 #include <compute/Compute_ParticleSys.hpp>
+#include <spdlog/fmt/fmt.h>
 
 namespace engine {
 
-          ParticleSysDataBuffer::ParticleSysDataBuffer(VulkanEngine* eng)
-                    :engine_(eng), staging(eng->allocator_), rndDist(0.f, 1.f), mt(rd())
-          {
-          }
+ParticleSysDataBuffer::ParticleSysDataBuffer(VulkanEngine *eng)
+    : engine_(eng), staging(eng->allocator_), rndDist(0.f, 1.f), mt(rd()) {}
 
-          ParticleSysDataBuffer::~ParticleSysDataBuffer() {
-                    destroy();
-          }
+ParticleSysDataBuffer::~ParticleSysDataBuffer() { destroy(); }
 
-          void ParticleSysDataBuffer::invalid() {
-                    destroy();
-                    pendingUpload_ = false;
-          }
-
-          bool ParticleSysDataBuffer::isValid() const { return isinit_; }
-
-          void   ParticleSysDataBuffer::destroy() {
-                    if (isinit_) {
-                              staging.destroy();
-                              for (auto& buf : buffers) {
-                                        buf->destroy();
-                                        buf.reset();
-                              }
-                              isinit_ = false;
-                    }
-          }
-
-          void ParticleSysDataBuffer::create(const std::size_t particle_count) {
-                    std::size_t bufferSize = sizeof(GPUParticle) * particle_count;
-
-                    if (isinit_) return;
-
-                    for (auto& buf : buffers) {
-                              buf.reset();
-                              buf = std::make_shared<AllocatedBuffer>(engine_->allocator_);
-                              buf->create(
-                                        bufferSize,
-                                        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
-                                        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
-                                        VK_BUFFER_USAGE_TRANSFER_DST_BIT |
-                                        VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
-
-                                        VMA_MEMORY_USAGE_GPU_ONLY, 
-                                        "ParticleSysDataBuffer::PingPongBuffer");
-                    }
-
-                    isinit_ = true;
-          }
-
-          void ParticleSysDataBuffer::submit(VkCommandBuffer cmd){
-
-                    staging.destroy();
-
-                    std::size_t bufferSize = sizeof(GPUParticle) * particleCount;
-
-                    /*create staging buffer for upload*/
-                    staging.create(bufferSize,
-                              VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                              VMA_MEMORY_USAGE_CPU_ONLY,
-                              "ParticleSysDataBuffer::StagingBuffer");
-
-                    //fill random data
-                    fill(staging.map(), particleCount);
-                    staging.unmap();
-
-                    VkBufferCopy bufferCopy{ 0,0,bufferSize };
-                    vkCmdCopyBuffer(cmd, staging.buffer, buffers[0]->buffer, 1, &bufferCopy);
-
-                    pendingUpload_ = true;
-          }
-
-          void ParticleSysDataBuffer::flush(VkFence fence) {
-                    if (!pendingUpload_)
-                              return;
-
-                    vkWaitForFences(engine_->device_, 1, &fence, true,
-                              std::numeric_limits<uint64_t>::max());
-
-                    // staging.destroy();
-                    pendingUpload_ = false;
-          }
-
-          void ParticleSysDataBuffer::fill(void* buffer, const std::size_t particle_count) {
-                    GPUParticle* src = reinterpret_cast<GPUParticle*>(buffer);
-                    for (std::size_t i = 0; i < particle_count; ++i) {
-                              auto& particle = src[i];
-
-                             const float r =  std::sqrtf(rndDist(mt));
-                             const float theta = 2.f * rndDist(mt) * glm::pi<float>();
-                             const float x = r * cosf(theta) * engine_->drawExtent_.height / engine_->drawExtent_.width;
-                             const float y = r * sinf(theta);
-                             particle.position = glm::vec4(x, y, 0, 0);
-
-                             glm::vec2 dir = glm::vec2(x, y);
-                             if (glm::length(dir) < 1e-6f) {
-                                       float angle = 2.f * rndDist(mt) * glm::pi<float>();
-                                       dir = glm::vec2(cosf(angle), sinf(angle));
-                             }
-
-                             particle.velocity = glm::vec4(normalize(dir) * 0.00025f, 0.f, 0.f);
-                             particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
-                    }
-          }
-
-          void ParticleSysDataBuffer::swap() noexcept {
-                    index ^= 1; // index = (index + 1) % 2
-          }
-
-          VkBuffer ParticleSysDataBuffer::get_in_buffer() const noexcept {
-                    return buffers[index]->buffer;
-          }
-
-          VkBuffer ParticleSysDataBuffer::get_out_buffer() const noexcept {
-                    return buffers[index ^ 1]->buffer;
-          }
+void ParticleSysDataBuffer::invalid() {
+  destroy();
+  pendingUpload_ = false;
 }
+
+bool ParticleSysDataBuffer::isValid() const { return isinit_; }
+
+void ParticleSysDataBuffer::destroy() {
+  if (isinit_) {
+    staging.destroy();
+    for (auto &buf : buffers) {
+      buf->destroy();
+      buf.reset();
+    }
+    isinit_ = false;
+  }
+}
+
+void ParticleSysDataBuffer::create(const std::size_t particle_count) {
+  std::size_t bufferSize = sizeof(GPUParticle) * particle_count;
+
+  if (isinit_)
+    return;
+
+  for (auto &buf : buffers) {
+    buf.reset();
+    buf = std::make_shared<AllocatedBuffer>(engine_->allocator_);
+    buf->create(
+        bufferSize,
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+            VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+            VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
+
+        VMA_MEMORY_USAGE_GPU_ONLY, "ParticleSysDataBuffer::PingPongBuffer");
+  }
+
+  isinit_ = true;
+}
+
+void ParticleSysDataBuffer::submit(VkCommandBuffer cmd) {
+
+  staging.destroy();
+
+  std::size_t bufferSize = sizeof(GPUParticle) * particleCount;
+
+  /*create staging buffer for upload*/
+  staging.create(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                 VMA_MEMORY_USAGE_CPU_ONLY,
+                 "ParticleSysDataBuffer::StagingBuffer");
+
+  // fill random data
+  fill(staging.map(), particleCount);
+  staging.unmap();
+
+  VkBufferCopy bufferCopy{0, 0, bufferSize};
+  vkCmdCopyBuffer(cmd, staging.buffer, buffers[0]->buffer, 1, &bufferCopy);
+
+  pendingUpload_ = true;
+}
+
+void ParticleSysDataBuffer::flush(VkFence fence) {
+  if (!pendingUpload_)
+    return;
+
+  vkWaitForFences(engine_->device_, 1, &fence, true,
+                  std::numeric_limits<uint64_t>::max());
+
+  // staging.destroy();
+  pendingUpload_ = false;
+}
+
+void ParticleSysDataBuffer::fill(void *buffer,
+                                 const std::size_t particle_count) {
+  GPUParticle *src = reinterpret_cast<GPUParticle *>(buffer);
+  for (std::size_t i = 0; i < particle_count; ++i) {
+    auto &particle = src[i];
+
+    const float r = std::sqrtf(rndDist(mt));
+    const float theta = 2.f * rndDist(mt) * glm::pi<float>();
+    const float x = r * cosf(theta) * engine_->drawExtent_.height /
+                    engine_->drawExtent_.width;
+    const float y = r * sinf(theta);
+    particle.position = glm::vec4(x, y, 0, 0);
+
+    glm::vec2 dir = glm::vec2(x, y);
+    if (glm::length(dir) < 1e-6f) {
+      float angle = 2.f * rndDist(mt) * glm::pi<float>();
+      dir = glm::vec2(cosf(angle), sinf(angle));
+    }
+
+    particle.velocity = glm::vec4(normalize(dir) * 0.00025f, 0.f, 0.f);
+    particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
+  }
+}
+
+void ParticleSysDataBuffer::swap() noexcept {
+  index ^= 1; // index = (index + 1) % 2
+}
+
+VkBuffer ParticleSysDataBuffer::get_in_buffer() const noexcept {
+  return buffers[index]->buffer;
+}
+
+VkBuffer ParticleSysDataBuffer::get_out_buffer() const noexcept {
+  return buffers[index ^ 1]->buffer;
+}
+} // namespace engine
