4e6aee9f73df8b937afce653167353ca2c826726
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-11-14 20:29:42 +1100

    1.[ADD]:add two dimension support to fill with random data 2.[UPDATE]:update algo and sample on sphere

--- vulkan-1.3/include/particle/ParticleDataBuffer.hpp
+++ vulkan-1.3/include/particle/ParticleDataBuffer.hpp
@@ -43,11 +43,17 @@ struct ParticleSysDataBuffer : std::false_type {
 template <typename ParticleType>
 struct ParticleSysDataBuffer<
     ParticleType, std::enable_if_t<has_particle_fields_v<ParticleType>, void>> {
-  ParticleSysDataBuffer(VkDevice device, VmaAllocator allocator)
-      : device_(device), allocator_(allocator), staging(allocator) {
+
+          enum class ParticleDimension {
+                    Particle2D, Particle3D
+          };
+
+  ParticleSysDataBuffer(VkDevice device, VmaAllocator allocator, ParticleDimension dim = ParticleDimension::Particle3D)
+      : device_(device), allocator_(allocator), staging(allocator) ,dim_(dim){
     mt.seed(static_cast<unsigned>(std::random_device{}()));
     rndDist = std::uniform_real_distribution<float>(0.f, 1.f);
   }
+
 
   virtual ~ParticleSysDataBuffer() { destroy(); }
 
@@ -101,8 +107,15 @@ struct ParticleSysDataBuffer<
   }
 
   void submit(VkCommandBuffer cmd) {
+
     // fill random data
-    fill(staging.map(), particleCount);
+            if (dim_ == ParticleDimension::Particle2D) {
+                      fill2D(staging.map(), particleCount);
+            }
+            else {
+                      fill3D(staging.map(), particleCount);
+            }
+
     staging.unmap();
 
     VkBufferCopy bufferCopy{0, 0, getBufferSize()};
@@ -154,34 +167,59 @@ protected:
   // This is the staging buffer to init the first buffer slot
   AllocatedBuffer staging;
 
-  void fill(void *buffer, const std::size_t particle_count) {
+  virtual void fill2D(void* buffer, const std::size_t particle_count) {
+            ParticleType* src = reinterpret_cast<ParticleType*>(buffer);
+            memset(src, 0, particle_count * particleSize);
+
+            for (std::size_t i = 0; i < particle_count; ++i) {
+                      auto& particle = src[i];
+
+                      float r = std::sqrt(rndDist(mt));
+                      float theta = 2.0f * glm::pi<float>() * rndDist(mt);
+
+                      float x = r * cos(theta);
+                      float y = r * sin(theta);
+
+                      particle.position = glm::vec4(x, y, 0, 1);
+                      glm::vec3 pos = glm::vec3(particle.position);
+                      glm::vec3 vel = glm::normalize(glm::cross(pos, glm::vec3(0, 0, 1)));
+                   
+                      particle.velocity = glm::vec4(vel * 0.002f, 0.0f);
+                      particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
+            }
+  }
+
+  virtual void fill3D(void *buffer, const std::size_t particle_count) {
     ParticleType *src = reinterpret_cast<ParticleType *>(buffer);
     memset(src, 0, particle_count * particleSize);
 
     for (std::size_t i = 0; i < particle_count; ++i) {
       auto &particle = src[i];
 
-      const float r = std::sqrtf(rndDist(mt));
-      const float theta = 2.f * rndDist(mt) * glm::pi<float>();
-      const float x = r * cosf(theta);
-      const float y = r * sinf(theta);
-      particle.position = glm::vec4(x, y, 0, 0);
+      float u = rndDist(mt);
+      float v = rndDist(mt);
 
-      glm::vec2 dir = glm::vec2(x, y);
-      if (glm::length(dir) < 1e-6f) {
-        float angle = 2.f * rndDist(mt) * glm::pi<float>();
-        dir = glm::vec2(cosf(angle), sinf(angle));
-      }
+      float theta = 2.0f * glm::pi<float>() * u;       // 0~2pi
+      float phi = acos(2.0f * v - 1);  //  0~¦Ð
 
-      particle.velocity = glm::vec4(normalize(dir) * 0.00025f, 0.f, 0.f);
+      float sx = sin(phi) * cos(theta);
+      float sy = sin(phi) * sin(theta);
+      float sz = cos(phi);
+
+      float r = cbrt(rndDist(mt));
+
+      glm::vec3 direction = glm::vec3(sx, sy, sz);
+      particle.position = glm::vec4(direction * r, 1.0f);
+      particle.velocity = glm::vec4(direction * 0.0005f, 0.f);
       particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
     }
   }
 
-private:
+protected:
   bool isinit_ = false;
   VkDevice device_{};
   VmaAllocator allocator_{};
+  ParticleDimension dim_;
   bool pendingUpload_ = false;
 };
 } // namespace engine
