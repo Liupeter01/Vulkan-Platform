df6d3e29bee0980c1376506cdc056e2f35cc95f3
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-11-21 12:15:14 +1100

    1.[MODIFY]:simplify timeline sem create process

--- vulkan-1.3/src/VulkanEngine.cpp
+++ vulkan-1.3/src/VulkanEngine.cpp
@@ -1077,16 +1077,7 @@ void VulkanEngine::init_frames(
   VkFenceCreateInfo fenceCreateInfo =
       tools::fence_create_info(VK_FENCE_CREATE_SIGNALED_BIT);
   VkSemaphoreCreateInfo semaphoreCreateInfo = tools::semaphore_create_info();
-
-  VkSemaphoreTypeCreateInfo timelineSemaphoreTypeCreateInfo{};
-  timelineSemaphoreTypeCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO;
-  timelineSemaphoreTypeCreateInfo.semaphoreType = VK_SEMAPHORE_TYPE_TIMELINE;
-  timelineSemaphoreTypeCreateInfo.initialValue = 0;
-
-  VkSemaphoreCreateInfo timelineSemaphoreCreateInfo{};
-  timelineSemaphoreCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-  timelineSemaphoreCreateInfo.pNext = reinterpret_cast<void*>(&timelineSemaphoreTypeCreateInfo);
-  timelineSemaphoreCreateInfo.flags = 0;
+  VkSemaphoreCreateInfo timelineSemaphoreCreateInfo = tools::timeline_semaphore_create_info();
 
   VkCommandPoolCreateInfo graphicCommandPoolInfo =
       tools::command_pool_create_info(
