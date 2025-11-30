b64762af1b9ea39ecbaa4593c4772556368ea085
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-11-30 21:39:44 +1100

    1.[ADD]:add QueueDispatcher structure with std::vector<Pack> normalPools_ 2.[ADD]:add dispatch function to generate index, especially when normalCounter == 1 3.[ADD]:add queueScheuler to store three types of QueueFlagBits support 4.[ADD]:add indepedant imgui & present queue 5.[ADD]:add extractQueuesByCategory() function to retrieve queuefamilyIndex & vkqueue 6.[ADD]:add finalizeByCategory(ALTHOGH ONLY DEAL WITH GRAPHIC), we have to satisfiy the avaiable amount of queue is more than special need and one normal queue

---
+++ vulkan-1.3/include/QueueScheduler.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#ifndef _QUEUE_SCHEDULER_HPP_
+#define _QUEUE_SCHEDULER_HPP_
+#include <vulkan/vulkan.hpp>
+#include <builder/MultiQueueDeviceBuilder.hpp>
+
+namespace engine {
+          using QueueRequest = typename MultiQueueDeviceBuilder::QueueRequest;
+          class QueueScheduler;
+
+          struct Pack {
+                    Pack() = default;
+                    Pack(uint32_t familyIndex, VkQueue q)
+                              :family(familyIndex), queue(q)
+                    { }
+                    uint32_t family{};
+                    VkQueue queue = VK_NULL_HANDLE;
+          };
+
+          struct QueueDispatcher {
+                    friend class QueueScheduler;
+
+                    QueueDispatcher(const VkQueueFlagBits queueType, std::vector<Pack>&& normalPool);
+                    Pack get_queue();
+                    virtual std::size_t dispatch();
+                    VkQueueFlagBits getQueueType() const;
+
+          protected:
+                    VkQueueFlagBits queueType_{};
+
+                    std::size_t normalIndex{  };
+                    std::size_t normalCounter{  };
+
+                    std::vector<Pack> normalPools_;
+          };
+
+          class QueueScheduler {
+                    using QueueRequestMappingStruct = std::unordered_map<VkQueueFlagBits, QueueRequest >;
+                    using QueueDispatcherMappingStruct = std::unordered_map<
+                              VkQueueFlagBits, 
+                              std::unique_ptr<QueueDispatcher> 
+                    >;
+
+          public:
+                    QueueScheduler(VkDevice device, const QueueRequestMappingStruct& request);
+
+                    Pack get_queue(VkQueueFlagBits flag);
+                    Pack imgui_queue();
+                    Pack present_queue();
+
+          protected:
+                    void finalizeByCategory(VkQueueFlagBits flag);
+                    void extractQueuesByCategory(VkQueueFlagBits flag);
+
+          protected:
+                    bool reserveImgui_ = false;
+                    bool reservePresent = false;
+                    //special queue support(Graphic Only)
+                    Pack imguiQueue_{};
+                    Pack presentQueue_{  };
+
+          private:
+                    QueueRequestMappingStruct queuePlan_;
+                    QueueDispatcherMappingStruct queueDispatcher_;
+                    VkDevice device_ = VK_NULL_HANDLE;
+          };
+}
+
+#endif //_QUEUE_SCHEDULER_HPP_
