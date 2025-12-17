e613281428fc7047faec42e009fc4115c01e4361
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-12-17 22:53:53 +1100

    1.[ADD]:add a dynamic file to instruct cmake to generate a static C++ macro as a header file to include by other header/source file

---
+++ vulkan-1.3/include/config.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define ENABLE_VALIDATION_LAYERS 0
+#define CONFIG_HOME "D:/Computer-Science/Software-Dev-Archieve/C++/Vulkan/"
+#define GLSL_SHADER_PATH "D:/Computer-Science/Software-Dev-Archieve/C++/Vulkan/shaders/glsl/"
+#define SLANG_SHADER_PATH  "D:/Computer-Science/Software-Dev-Archieve/C++/Vulkan/shaders/slang/"
+
+#endif //_CONFIG_H_
