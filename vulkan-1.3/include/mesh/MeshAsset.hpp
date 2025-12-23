94c40a7cbcb4512775e8a807302093eef615d26c
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-12-23 21:00:37 +1100

    1.[ADD & NOT TESTED]:add version2 MeshAsset2 and delegate v2::GPUGeoMeshBuffers2's interfaces

--- vulkan-1.3/include/mesh/MeshAsset.hpp
+++ vulkan-1.3/include/mesh/MeshAsset.hpp
@@ -40,6 +40,41 @@ struct MeshAsset {
   std::vector<GeoSurface> meshSurfaces;
   GPUGeoMeshBuffers meshBuffers;
 };
+
+namespace v1 {}
+
+namespace v2 {
+          struct MeshAsset2 {
+                    MeshAsset2(VkDevice device, VmaAllocator allocator)
+                              : meshBuffers{ device, allocator } {
+                    }
+
+                    void createAsset(std::vector<Vertex>&& vertices,
+                              std::vector<uint32_t>&& indices) {
+                              meshBuffers.createMesh(std::move(vertices), std::move(indices));
+                    }
+
+                    void recordUpload(VkCommandBuffer cmd) {
+                              meshBuffers.recordUpload(cmd);
+                    }
+
+                    void setUploadCompleteTimeline(uint64_t value) {
+                              meshBuffers.setUploadCompleteTimeline(value);
+                    }
+
+                    void purgeReleaseStaging(uint64_t observedValue) {
+                              meshBuffers.purgeReleaseStaging(observedValue);
+                    }
+
+                    VkDeviceAddress getVertexDeviceAddr() {
+                              return meshBuffers.getVertexBufferDeviceAddress();
+                    }
+
+                    std::string meshName;
+                    std::vector<GeoSurface> meshSurfaces;
+                    v2::GPUGeoMeshBuffers2 meshBuffers;
+          };
+}
 } // namespace mesh
 } // namespace engine
 
