addf8b94f5ceb779070f270f32e88c8714afc6df
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-12-29 12:58:43 +1100

    1.[UPDATE]:update allocatebuffer class submission code, user could integrate their particle buffer submission by using trasnfer queue, instead of using imm submit

--- vulkan-1.3/include/particle/ParticleDataBuffer.hpp
+++ vulkan-1.3/include/particle/ParticleDataBuffer.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <vulkan/vulkan_core.h>
 #ifndef _PARTICLE_DATA_BUFFER_HPP_
 #define _PARTICLE_DATA_BUFFER_HPP_
 #include <GlobalDef.hpp>
@@ -7,6 +6,9 @@
 #include <memory>
 #include <random>
 #include <type_traits>
+#include <vulkan/vulkan.h>
+#include <AllocatedBuffer.hpp>
+#include <atomic>
 
 namespace engine {
 
@@ -40,113 +42,51 @@ struct ParticleSysDataBuffer : std::false_type {
                 "velocity, and color fields!");
 };
 
-template <typename ParticleType>
+// ParticleSysDataBuffer implements ping-pong GPU storage for particle simulation.
+// - Two GPU buffers are allocated: buffers_[0] and buffers_[1].
+// - get_in_buffer() reads from buffers_[index], get_out_buffer() writes to buffers_[index ^ 1].
+// - swap() flips index each simulation step.
+//
+// Upload model:
+// - perpareTransferData(): creates buffers if needed and prepares CPU staging for BOTH buffers,
+//   so both ping-pong states start initialized (avoids reading uninitialized GPU memory).
+// - singleTimeUpload(cmd): records GPU upload commands once (idempotent).
+// - updateUploadingStatus(observedValue): advances internal resource state based on timeline.
+// - purgeReleaseStaging(observedValue): releases CPU staging only after GPU upload is complete.
+
+template <typename _Ty>
 struct ParticleSysDataBuffer<
-    ParticleType, std::enable_if_t<has_particle_fields_v<ParticleType>, void>> {
+          _Ty, std::enable_if_t<has_particle_fields_v<_Ty>, void>> {
+
+          using ParticleType = _Ty;
 
   enum class ParticleDimension { Particle2D, Particle3D };
 
-  ParticleSysDataBuffer(VkDevice device, VmaAllocator allocator,
-                        ParticleDimension dim = ParticleDimension::Particle3D)
-      : device_(device), allocator_(allocator), staging(allocator), dim_(dim) {
-    mt.seed(static_cast<unsigned>(std::random_device{}()));
-    rndDist = std::uniform_real_distribution<float>(0.f, 1.f);
-  }
+  ParticleSysDataBuffer(VkDevice device, VmaAllocator allocator);
+  virtual ~ParticleSysDataBuffer();
 
-  virtual ~ParticleSysDataBuffer() { destroy(); }
+  void destroy();
 
-  void destroy() {
-    if (isinit_) {
-      staging.destroy();
-      for (auto &buf : buffers) {
-        buf->destroy();
-        buf.reset();
-      }
-      isinit_ = false;
-    }
-  }
+  void configure(const std::size_t particle_count,
+                    ParticleDimension dim = ParticleDimension::Particle3D,
+                      VkBufferUsageFlags bufferUsage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
+                                                                                  VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+                                                                                  VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+                                                                                  VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
+                      VmaMemoryUsage memoryUsage = VMA_MEMORY_USAGE_GPU_ONLY
+                     );
 
-  void invalid() {
-    destroy();
-    pendingUpload_ = false;
-  }
+  void singleTimeUpload(VkCommandBuffer cmd);
+  void perpareTransferData();
+  void updateUploadingStatus(uint64_t observedValue);
+  void purgeReleaseStaging(uint64_t observedValue);
+  void setUploadCompleteTimeline(uint64_t value);
 
-  bool isValid() const { return isinit_; }
-
-  void create(const std::size_t particle_count) {
-    if (isinit_)
-      return;
-
-    staging.destroy();
-
-    particleCount = particle_count;
-    const std::size_t bufferSize = particleSize * particle_count;
-
-    /*create staging buffer for upload*/
-    staging.create(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
-                   VMA_MEMORY_USAGE_CPU_ONLY,
-                   "ParticleSysDataBuffer::StagingBuffer");
-
-    /*Ping-Pong Swap Buffer*/
-    for (auto &buf : buffers) {
-      buf.reset();
-      buf = std::make_shared<::engine::v1::AllocatedBuffer>(allocator_);
-      buf->create(bufferSize,
-                  VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
-                      VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
-                      VK_BUFFER_USAGE_TRANSFER_DST_BIT |
-                      VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
-
-                  VMA_MEMORY_USAGE_GPU_ONLY,
-                  "ParticleSysDataBuffer::PingPongBuffer");
-    }
-
-    isinit_ = true;
-  }
-
-  void submit(VkCommandBuffer cmd) {
-
-    // fill random data
-    if (dim_ == ParticleDimension::Particle2D) {
-      fill2D(staging.map(), particleCount);
-    } else {
-      fill3D(staging.map(), particleCount);
-    }
-
-    staging.unmap();
-
-    VkBufferCopy bufferCopy{0, 0, getBufferSize()};
-    vkCmdCopyBuffer(cmd, staging.buffer, buffers[0]->buffer, 1, &bufferCopy);
-
-    pendingUpload_ = true;
-  }
-
-  void flush(VkFence fence) {
-    if (!pendingUpload_)
-      return;
-
-    vkWaitForFences(device_, 1, &fence, true,
-                    std::numeric_limits<uint64_t>::max());
-
-    // staging.destroy();
-    pendingUpload_ = false;
-  }
-
-  std::size_t getParticleCount() const noexcept { return particleCount; }
-
-  std::size_t getBufferSize() const noexcept {
-    return particleCount * particleSize;
-  }
-
-  void swap() noexcept {
-    index ^= 1; // index = (index + 1) % 2
-  }
-
-  VkBuffer get_in_buffer() const noexcept { return buffers[index]->buffer; }
-
-  VkBuffer get_out_buffer() const noexcept {
-    return buffers[index ^ 1]->buffer;
-  }
+  std::size_t getParticleCount() const noexcept;
+  std::size_t getBufferSize() const noexcept;
+  void swap() noexcept;
+  VkBuffer get_in_buffer() const noexcept;
+  VkBuffer get_out_buffer() const noexcept;
 
   /*one is for particle in, another is for particle out;
    * buffer[0] will be the first one and it's going to swap in every frame
@@ -154,70 +94,48 @@ struct ParticleSysDataBuffer<
   bool index = 0;
   std::size_t particleCount{0};
   std::size_t particleSize = sizeof(ParticleType);
-  std::array<std::shared_ptr<::engine::v1::AllocatedBuffer>, 2> buffers;
+  std::size_t particleBufferSize{ 0 };
+
+  std::array<std::shared_ptr< ::engine::v2::AllocatedBuffer2>, 2> buffers_;
+
+  //struct {
+  //          std::array<std::shared_ptr< ::engine::v2::AllocatedBuffer2>, 2> position_;
+  //          std::array<std::shared_ptr< ::engine::v2::AllocatedBuffer2>, 2> velocity_;
+  //          std::array<std::shared_ptr< ::engine::v2::AllocatedBuffer2>, 2> color_;
+  //} ParticleStorge;
 
 protected:
+          virtual std::vector<uint8_t> fill2D();
+          virtual std::vector<uint8_t> fill3D();
+
+private:
+          void __createGpuBuffer();
+          void __destroyGpuBuffer();
+
+protected:
+  ParticleDimension dim_;
+
   std::random_device rd;
   std::mt19937 mt;
   std::uniform_real_distribution<float> rndDist;
 
-  // This is the staging buffer to init the first buffer slot
-  ::engine::v1::AllocatedBuffer staging;
+  VkBufferUsageFlags bufferUsage_ = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
+            VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+            VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+            VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT;
+  VmaMemoryUsage memoryUsage_ = VMA_MEMORY_USAGE_GPU_ONLY;
 
-  virtual void fill2D(void *buffer, const std::size_t particle_count) {
-    ParticleType *src = reinterpret_cast<ParticleType *>(buffer);
-    memset(src, 0, particle_count * particleSize);
+private:
+          bool configure_ = false;
+          bool gpuAllocated_ = false;
+          bool uploadOnceFlag_ = false;
+          bool cpuReady_ = false;
+          bool pendingUpload_ = false;
 
-    for (std::size_t i = 0; i < particle_count; ++i) {
-      auto &particle = src[i];
+  std::once_flag upload_once_;
 
-      float r = std::sqrt(rndDist(mt));
-      float theta = 2.0f * glm::pi<float>() * rndDist(mt);
-
-      float x = r * cos(theta);
-      float y = r * sin(theta);
-
-      particle.position = glm::vec4(x, y, 0, 1);
-      glm::vec3 pos = glm::vec3(particle.position);
-      glm::vec3 vel = glm::normalize(glm::cross(pos, glm::vec3(0, 0, 1)));
-
-      particle.velocity = glm::vec4(vel * 0.002f, 0.0f);
-      particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
-    }
-  }
-
-  virtual void fill3D(void *buffer, const std::size_t particle_count) {
-    ParticleType *src = reinterpret_cast<ParticleType *>(buffer);
-    memset(src, 0, particle_count * particleSize);
-
-    for (std::size_t i = 0; i < particle_count; ++i) {
-      auto &particle = src[i];
-
-      float u = rndDist(mt);
-      float v = rndDist(mt);
-
-      float theta = 2.0f * glm::pi<float>() * u; // 0~2pi
-      float phi = acos(2.0f * v - 1);            //  0~¦Ð
-
-      float sx = sin(phi) * cos(theta);
-      float sy = sin(phi) * sin(theta);
-      float sz = cos(phi);
-
-      float r = cbrt(rndDist(mt));
-
-      glm::vec3 direction = glm::vec3(sx, sy, sz);
-      particle.position = glm::vec4(direction * r, 1.0f);
-      particle.velocity = glm::vec4(direction * 0.0005f, 0.f);
-      particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
-    }
-  }
-
-protected:
-  bool isinit_ = false;
   VkDevice device_{};
   VmaAllocator allocator_{};
-  ParticleDimension dim_;
-  bool pendingUpload_ = false;
 };
 } // namespace engine
 
---
+++ vulkan-1.3/src/ParticleSysDataBuffer.cpp
@@ -0,0 +1,217 @@
+#include <particle/PointSpriteParticleSystemBase.hpp>
+#include <particle/ParticleDataBuffer.hpp>
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/ext/scalar_constants.hpp>
+
+namespace engine {
+
+          template<>
+          ParticleSysDataBuffer< ::engine::particle::GPUParticle>::ParticleSysDataBuffer(VkDevice device, VmaAllocator allocator)
+                    : device_(device), allocator_(allocator){
+                    mt.seed(static_cast<unsigned>(std::random_device{}()));
+                    rndDist = std::uniform_real_distribution<float>(0.f, 1.f);
+          }
+
+          template<>
+          ParticleSysDataBuffer< ::engine::particle::GPUParticle>::~ParticleSysDataBuffer() { destroy(); }
+
+          template<>
+          void  ParticleSysDataBuffer< ::engine::particle::GPUParticle>::destroy() {
+                    __destroyGpuBuffer();
+          }
+
+          template<>
+          void  ParticleSysDataBuffer< ::engine::particle::GPUParticle>::configure(const std::size_t particle_count,
+                                                                                                                                  ParticleDimension dim ,
+                                                                                                                                  VkBufferUsageFlags bufferUsage,
+                                                                                                                                  VmaMemoryUsage memoryUsage) {
+
+                    if (gpuAllocated_) {
+                              throw std::runtime_error("configure() after GPU allocated; call destroy() first");
+                    }
+
+                    dim_ = dim;
+
+                    particleCount = particle_count;
+                    particleBufferSize = particleCount * particleSize;
+
+                    bufferUsage_ = bufferUsage;
+                    memoryUsage_ = memoryUsage;
+
+                    configure_ = true;
+          }
+
+          template<>
+          std::size_t ParticleSysDataBuffer< ::engine::particle::GPUParticle>::getParticleCount() const noexcept { 
+                    //if (!configure_)
+                    //          throw std::runtime_error("Please configure() first");
+                    return particleCount;
+          }
+
+          template<>
+          std::size_t ParticleSysDataBuffer< ::engine::particle::GPUParticle>::getBufferSize() const noexcept {
+                    //if (!configure_)
+                    //          throw std::runtime_error("Please configure() first");
+                    return particleBufferSize;
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::swap() noexcept {
+                    index ^= 1; // index = (index + 1) % 2
+          }
+
+          template<>
+          VkBuffer ParticleSysDataBuffer< ::engine::particle::GPUParticle>::get_in_buffer() const noexcept { 
+                    //if (!buffers_[index]) {
+                    //          throw std::runtime_error("ParticleSysDataBuffer: Invalid Buffer");
+                    //}
+                    return buffers_[index]->buffer(); 
+          }
+
+          template<>
+          VkBuffer ParticleSysDataBuffer< ::engine::particle::GPUParticle>::get_out_buffer() const noexcept {
+                    //if (!buffers_[index ^ 1]) {
+                    //          throw std::runtime_error("ParticleSysDataBuffer: Invalid Buffer");
+                    //}
+                    return buffers_[index ^ 1]->buffer();
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::__createGpuBuffer(){
+                    if (gpuAllocated_)
+                              return;
+
+                    for (auto& buf : buffers_) {
+                              buf.reset();
+                              buf = std::make_shared<::engine::v2::AllocatedBuffer2>(device_, allocator_,
+                                        "ParticleSysDataBuffer::PingPongBuffer");
+
+                              buf->configure(particleBufferSize, bufferUsage_, memoryUsage_);
+                    }
+
+                    gpuAllocated_ = true;
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::__destroyGpuBuffer() {
+                    if (gpuAllocated_) {
+                              for (auto& buf : buffers_) {
+                                        buf->destroy();
+                                        buf.reset();
+                              }
+                              gpuAllocated_ = false;
+                    }
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::singleTimeUpload(VkCommandBuffer cmd) {
+
+                    std::call_once(upload_once_, [&] {
+                              if (!cpuReady_) 
+                                        throw std::runtime_error("CPU staging missing");
+                                        for (auto& buf : buffers_) buf->recordUpload(cmd);
+                              pendingUpload_ = true;
+                              });
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::perpareTransferData() {
+
+                    if (!configure_)
+                              throw std::runtime_error("Please configure() first");
+
+                    if (!gpuAllocated_) {
+                              __createGpuBuffer(); 
+                    }
+                    
+                    std::vector<uint8_t> data = ((dim_ == ParticleDimension::Particle2D) ? fill2D() : fill3D());
+
+                    for (auto& buf : buffers_) {
+                              buf->perpareTransferData(data.data(), data.size());
+                    }
+                    cpuReady_ = true;
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::updateUploadingStatus(uint64_t observedValue){
+                    if (pendingUpload_) {
+                              for (auto& buf : buffers_) {
+                                        buf->updateUploadingStatus(observedValue);
+                              }
+                              pendingUpload_ = false;
+                    }
+          }
+
+          template<>
+          void ParticleSysDataBuffer< ::engine::particle::GPUParticle>::purgeReleaseStaging(uint64_t observedValue){
+                    
+                    if (cpuReady_) {
+                              for (auto& buf : buffers_) {
+                                        buf->purgeReleaseStaging(observedValue);
+                              }
+                              cpuReady_ = false;
+                    }
+          }
+
+          template<>
+          void  ParticleSysDataBuffer< ::engine::particle::GPUParticle>::setUploadCompleteTimeline(uint64_t value) {
+                    for (auto& buf : buffers_) {
+                              buf->setUploadCompleteTimeline(value);
+                    }
+          }
+
+          template<>
+          std::vector<uint8_t> ParticleSysDataBuffer< ::engine::particle::GPUParticle>::fill2D() {
+                    std::vector<uint8_t> ret;
+                    ret.resize(particleBufferSize);
+                    ParticleType* src = reinterpret_cast<ParticleType*>(ret.data());
+
+                    for (std::size_t i = 0; i < particleCount; ++i) {
+                              auto& particle = src[i];
+
+                              float r = std::sqrt(rndDist(mt));
+                              float theta = 2.0f * glm::pi<float>() * rndDist(mt);
+
+                              float x = r * cos(theta);
+                              float y = r * sin(theta);
+
+                              particle.position = glm::vec4(x, y, 0, 1);
+                              glm::vec3 pos = glm::vec3(particle.position);
+                              glm::vec3 vel = glm::normalize(glm::cross(pos, glm::vec3(0, 0, 1)));
+
+                              particle.velocity = glm::vec4(vel * 0.002f, 0.0f);
+                              particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
+                    }
+                    return ret;
+          }
+
+          template<>
+          std::vector<uint8_t> ParticleSysDataBuffer< ::engine::particle::GPUParticle>::fill3D(){
+                    std::vector<uint8_t> ret;
+                    ret.resize(particleBufferSize);
+                    ParticleType* src = reinterpret_cast<ParticleType*>(ret.data());
+                    for (std::size_t i = 0; i < particleCount; ++i) {
+                              auto& particle = src[i];
+
+                              float u = rndDist(mt);
+                              float v = rndDist(mt);
+
+                              float theta = 2.0f * glm::pi<float>() * u; // 0~2pi
+                              float phi = acos(2.0f * v - 1);            //  0~¦Ð
+
+                              float sx = sin(phi) * cos(theta);
+                              float sy = sin(phi) * sin(theta);
+                              float sz = cos(phi);
+
+                              float r = cbrt(rndDist(mt));
+
+                              glm::vec3 direction = glm::vec3(sx, sy, sz);
+                              particle.position = glm::vec4(direction * r, 1.0f);
+                              particle.velocity = glm::vec4(direction * 0.0005f, 0.f);
+                              particle.color = glm::vec4(rndDist(mt), rndDist(mt), rndDist(mt), 1.0f);
+                    }
+                    return ret;
+          }
+}
